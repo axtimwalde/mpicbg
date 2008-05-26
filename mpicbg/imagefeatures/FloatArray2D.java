2c9a7360711378fca0d6efab95cad2c7b34d8d7e
Stephan <saalfeld@mpi-cbg.de>
2008-05-26 15:06:49 +0200

    Removed stuff that I do not need from FloatArray2D.java

--- mpicbg/imagefeatures/FloatArray2D.java
+++ mpicbg/imagefeatures/FloatArray2D.java
@@ -1,14 +1,8 @@
 package mpicbg.imagefeatures;
 
 /**
- * <p>Title: PhaseCorrelation2D</p>
- *
- * <p>Description: </p>
- *
- * <p>Copyright: Copyright (c) 2007</p>
- *
- * <p>Company: </p>
- *
+ * Simple 2d float array that stores all values in one linear array.
+ * 
  * <p>License: GPL
  *
  * This program is free software; you can redistribute it and/or
@@ -24,107 +18,42 @@ package mpicbg.imagefeatures;
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * @author Stephan Preibisch
- * @version 1.0
+ * @author Stephan Preibisch (and Stephan Saalfeld the deleter)
+ * @version 0.2b
  */
 public class FloatArray2D extends FloatArray
 {
-//    public float data[] = null;
-    public int width = 0;
-    public int height = 0;
+	final public int width;
+	final public int height;
 
+	public FloatArray2D( int width, int height )
+	{
+		data = new float[ width * height ];
+		this.width = width;
+		this.height = height;
+	}
 
-    public FloatArray2D(int width, int height)
-    {
-        data = new float[width * height];
-        this.width = width;
-        this.height = height;
-    }
+	public FloatArray2D( float[] data, int width, int height )
+	{
+		this.data = data;
+		this.width = width;
+		this.height = height;
+	}
 
-    public FloatArray2D(float[] data, int width, int height)
-    {
-        this.data = data;
-        this.width = width;
-        this.height = height;
-    }
+	public FloatArray2D clone()
+	{
+		FloatArray2D clone = new FloatArray2D( width, height );
+		System.arraycopy( this.data, 0, clone.data, 0, this.data.length );
+		return clone;
+	}
 
-    public FloatArray2D clone()
-    {
-        FloatArray2D clone = new FloatArray2D(width, height);
-        System.arraycopy(this.data, 0, clone.data, 0, this.data.length);
-        return clone;
-    }
+	final public float get( int x, int y )
+	{
+		return data[ y * width + x ];
+	}
 
-    public int getPos(int x, int y)
-    {
-        return x + width * y;
-    }
-
-    public float get(int x, int y)
-    {
-        return data[getPos(x,y)];
-    }
-
-    public float getMirror(int x, int y)
-    {
-        if (x >= width)
-            x = width - (x - width + 2);
-
-        if (y >= height)
-            y = height - (y - height + 2);
-
-        if (x < 0)
-        {
-            int tmp = 0;
-            int dir = 1;
-
-            while (x < 0)
-            {
-                tmp += dir;
-                if (tmp == width - 1 || tmp == 0)
-                    dir *= -1;
-                x++;
-            }
-            x = tmp;
-        }
-
-        if (y < 0)
-        {
-            int tmp = 0;
-            int dir = 1;
-
-            while (y < 0)
-            {
-                tmp += dir;
-                if (tmp == height - 1 || tmp == 0)
-                    dir *= -1;
-                y++;
-            }
-            y = tmp;
-        }
-
-       return data[getPos(x,y)];
-    }
-
-    public float getZero(int x, int y)
-    {
-        if (x >= width)
-            return 0;
-
-        if (y >= height)
-            return 0;
-
-        if (x < 0)
-            return 0;
-
-        if (y < 0)
-            return 0;
-
-        return data[getPos(x,y)];
-    }
-
-    public void set(float value, int x, int y)
-    {
-        data[getPos(x,y)] = value;
-    }
+	final public void set( float value, int x, int y )
+	{
+		data[ y * width + x ] = value;
+	}
 }
