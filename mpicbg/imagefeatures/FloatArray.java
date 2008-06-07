eb49b86c7a649275a5cd67fb35f4f9bbc47401fd
Stephan <saalfeld@mpi-cbg.de>
2008-06-07 14:43:17 +0200

    Added proper GPL comments

--- mpicbg/imagefeatures/FloatArray.java
+++ mpicbg/imagefeatures/FloatArray.java
@@ -1,16 +1,25 @@
 package mpicbg.imagefeatures;
 
 /**
- * <p>Title: </p>
+ * Simple float array that stores all values in one linear array.
+ * 
+ * <p>License: GPL
  *
- * <p>Description: </p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
  *
- * <p>Copyright: Copyright (c) 2006</p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * <p>Company: </p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * @author not attributable
- * @version 1.0
+ * @author Stephan Preibisch
+ * @version 0.2b
  */
 public abstract class FloatArray
 {
