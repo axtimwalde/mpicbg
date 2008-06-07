55bb2aee02038fa8795fa3063bdb4f0965d6c44d
Stephan <saalfeld@mpi-cbg.de>
2008-06-07 15:09:47 +0200

    Added the closest/next closest parameter

--- SIFT_ExtractPointRoi.java
+++ SIFT_ExtractPointRoi.java
@@ -81,6 +81,8 @@ public class SIFT_ExtractPointRoi implements PlugIn, KeyListener
 	private static int fdsize = 4;
 	// feature descriptor orientation bins
 	private static int fdbins = 8;
+	// closest/next closest neighbour distance ratio
+	private static float rod = 0.92f;
 	// size restrictions for scale octaves, use octaves < max_size and > min_size only
 	private static int min_size = 64;
 	private static int max_size = 1024;
@@ -131,6 +133,7 @@ public class SIFT_ExtractPointRoi implements PlugIn, KeyListener
 		gd.addNumericField( "feature_descriptor_orientation_bins :", fdbins, 0 );
 		gd.addNumericField( "minimum_image_size :", min_size, 0 );
 		gd.addNumericField( "maximum_image_size :", max_size, 0 );
+		gd.addNumericField( "closest/next_closest_ratio :", rod, 2 );
 		gd.addNumericField( "maximal_alignment_error :", max_epsilon, 2 );
 		gd.addNumericField( "inlier_ratio :", min_inlier_ratio, 2 );
 		gd.addCheckbox( "upscale_image_first", upscale );
@@ -148,6 +151,7 @@ public class SIFT_ExtractPointRoi implements PlugIn, KeyListener
 		fdbins = ( int )gd.getNextNumber();
 		min_size = ( int )gd.getNextNumber();
 		max_size = ( int )gd.getNextNumber();
+		rod = ( float )gd.getNextNumber();
 		max_epsilon = ( float )gd.getNextNumber();
 		min_inlier_ratio = ( float )gd.getNextNumber();
 		upscale = gd.getNextBoolean();
@@ -207,7 +211,7 @@ public class SIFT_ExtractPointRoi implements PlugIn, KeyListener
 		IJ.log( "Identifying correspondence candidates using brute force ..." );
 		Vector< PointMatch > candidates = 
 				//FloatArray2DSIFT.createMatches( fs1, fs2, 100.0f, null, Float.MAX_VALUE );
-				FloatArray2DSIFT.createMatches( fs1, fs2 );
+				FloatArray2DSIFT.createMatches( fs1, fs2, rod );
 		IJ.log( " took " + ( System.currentTimeMillis() - start_time ) + "ms." );	
 		IJ.log( candidates.size() + " potentially corresponding features identified." );
 			
