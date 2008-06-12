435d3abf4a73f9477227c0c693ed043b0ba67e30
Stephan <saalfeld@mpi-cbg.de>
2008-06-12 09:38:12 +0200

    Added trangular mesh transform plugin

---
+++ Transform_TriangularGrid.java
@@ -0,0 +1,280 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.plugin.PlugIn;
+import ij.process.*;
+import ij.gui.*;
+
+import mpicbg.models.*;
+
+import java.awt.Event;
+import java.awt.TextField;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+public class Transform_TriangularGrid implements PlugIn, MouseListener,  MouseMotionListener, KeyListener
+{
+	// number of x-handles
+	private static int numX = 4;
+	// number of x-handles
+	private static int numY = 3;
+	
+	ImagePlus imp;
+	ImageProcessor ip;
+	ImageProcessor ipOrig;
+	
+	PointMatch[] pq;
+	int[] x;
+	int[] y;
+	PointRoi handles;
+	
+	final TransformMesh mt = new TransformMesh();
+	
+	int targetIndex = -1;
+	
+	public void run( String arg )
+    {
+		// cleanup
+		mt.clear();
+		
+		imp = IJ.getImage();
+		ip = imp.getProcessor();
+		ipOrig = ip.duplicate();
+		
+		GenericDialog gd = new GenericDialog( "Grid Transform" );
+		gd.addNumericField( "horizontal_handles :", numX, 0 );
+		gd.addNumericField( "vertical_handles :", numY, 0 );
+		gd.showDialog();
+		
+		if (gd.wasCanceled()) return;
+		
+		numX = ( int )gd.getNextNumber();
+		numY = ( int )gd.getNextNumber();
+		
+		// intitialize the transform mesh
+		initMesh( numX, numY );
+		
+		
+		handles = new PointRoi( x, y, x.length );
+		imp.setRoi( handles );
+		
+		Toolbar.getInstance().setTool( Toolbar.getInstance().addTool( "Drag_the_handles." ) );
+		
+		imp.getCanvas().addMouseListener( this );
+		imp.getCanvas().addMouseMotionListener( this );
+		imp.getCanvas().addKeyListener( this );
+    }
+	
+	public void initMesh( int numX, int numY )
+	{
+		pq = new PointMatch[ numX * numY + ( numX - 1 ) * ( numY - 1 ) ];
+		x = new int[ pq.length ];
+		y = new int[ pq.length ];
+		
+		float dy = ( float )ip.getHeight() / ( numY - 1 );
+		float dx = ( float )ip.getWidth() / ( numX - 1 );
+		
+		int i = 0;
+		for ( int xi = 0; xi < numX; ++xi )
+		{
+			float xip = xi * dx;
+			Point p = new Point( new float[]{ xip, 0 } );
+			pq[ i ]  = new PointMatch( p, p.clone() );
+			
+			x[ i ] = ( int )( xip );
+			y[ i ] = 0;
+			
+			++i;
+		}
+		for ( int yi = 1; yi < numY; ++yi )
+		{
+			// odd row
+			float yip = yi * dy - dy / 2;
+			for ( int xi = 1; xi < numX; ++xi )
+			{
+				float xip = xi * dx - dx / 2;
+				
+				Point p  = new Point( new float[]{ xip, yip } );
+				pq[ i ] = new PointMatch( p, p.clone() );
+				
+				x[ i ] = ( int )( xip);
+				y[ i ] = ( int )( yip);
+				
+				int i1 = i - numX;
+				int i2 = i1 + 1;
+				
+				ArrayList< PointMatch > t1 = new ArrayList< PointMatch >();
+				t1.add( pq[ i1 ] );
+				t1.add( pq[ i2 ] );
+				t1.add( pq[ i ] );
+				
+				mt.addTriangle( t1 );
+				
+				++i;
+			}
+			
+			// even row
+			yip = yi * dy;
+			Point p  = new Point( new float[]{ 0, yip } );
+			pq[ i ] = new PointMatch( p, p.clone() );
+			
+			x[ i ] = ( int )( 0 );
+			y[ i ] = ( int )( yip );
+			
+			++i;
+			
+			for ( int xi = 1; xi < numX; ++xi )
+			{
+				float xip = xi * dx;
+								
+				p = new Point( new float[]{ xip, yip } );
+				pq[ i ] = new PointMatch( p, p.clone() );
+				
+				x[ i ] = ( int )( xip );
+				y[ i ] = ( int )( yip );
+				
+				int i1 = i - 2 * numX;
+				int i2 = i1 + 1;
+				int i3 = i1 + numX;
+				int i4 = i - 1;
+				
+				ArrayList< PointMatch > t1 = new ArrayList< PointMatch >();
+				t1.add( pq[ i1 ] );
+				t1.add( pq[ i3 ] );
+				t1.add( pq[ i4 ] );
+				
+				ArrayList< PointMatch > t2 = new ArrayList< PointMatch >();
+				t2.add( pq[ i4 ] );
+				t2.add( pq[ i3 ] );
+				t2.add( pq[ i ] );
+				
+				ArrayList< PointMatch > t3 = new ArrayList< PointMatch >();
+				t3.add( pq[ i ] );
+				t3.add( pq[ i3 ] );
+				t3.add( pq[ i2 ] );
+				
+				mt.addTriangle( t1 );
+				mt.addTriangle( t2 );
+				mt.addTriangle( t3 );
+				
+				++i;
+			}
+		}
+	}
+	
+	public void keyPressed( KeyEvent e)
+	{
+		if ( e.getKeyCode() == KeyEvent.VK_ESCAPE || e.getKeyCode() == KeyEvent.VK_ENTER )
+		{
+			if ( imp != null )
+			{
+				imp.getCanvas().removeMouseListener( this );
+				imp.getCanvas().removeMouseMotionListener( this );
+				imp.getCanvas().removeKeyListener( this );
+				imp.getCanvas().setDisplayList( null );
+				imp.setRoi( ( Roi )null );
+			}
+			if ( e.getKeyCode() == KeyEvent.VK_ESCAPE )
+			{
+				imp.setProcessor( null, ipOrig );
+			}
+		}
+		else if (
+				( e.getKeyCode() == KeyEvent.VK_F1 ) &&
+				( e.getSource() instanceof TextField ) ){}
+	}
+
+	public void keyReleased( KeyEvent e ){}
+	public void keyTyped( KeyEvent e ){}
+	
+	public void mousePressed( MouseEvent e )
+	{
+		targetIndex = -1;
+		if ( e.getButton() == MouseEvent.BUTTON1 )
+		{
+			ImageWindow win = WindowManager.getCurrentWindow();
+			int x = win.getCanvas().offScreenX( e.getX() );
+			int y = win.getCanvas().offScreenY( e.getY() );
+			
+			double target_d = Double.MAX_VALUE;
+			for ( int i = 0; i < pq.length; ++i )
+			{
+				double dx = win.getCanvas().getMagnification() * ( pq[ i ].getP2().getW()[ 0 ] - x );
+				double dy = win.getCanvas().getMagnification() * ( pq[ i ].getP2().getW()[ 1 ]  - y );
+				double d =  dx * dx + dy * dy;
+				if ( d < 64.0 && d < target_d )
+				{
+					targetIndex = i;
+					target_d = d;
+				}
+			}
+		}
+		
+		//IJ.log( "Mouse pressed: " + x + ", " + y + " " + modifiers( e.getModifiers() ) );
+	}
+
+	public void mouseReleased( MouseEvent e ){}
+	public void mouseExited( MouseEvent e ) {}
+	public void mouseClicked( MouseEvent e ) {}	
+	public void mouseEntered( MouseEvent e ) {}
+	
+	public void mouseDragged( MouseEvent e )
+	{
+		if ( targetIndex >= 0 )
+		{
+			ImageWindow win = WindowManager.getCurrentWindow();
+			int x = win.getCanvas().offScreenX( e.getX() );
+			int y = win.getCanvas().offScreenY( e.getY() );
+			
+			float[] fq = pq[ targetIndex ].getP2().getW();
+			
+			int[] rx = new int[ pq.length ];
+			int[] ry = new int[ pq.length ];
+			
+			for ( int i = 0; i < pq.length; ++i )
+			{
+				rx[ i ] = ( int )pq[ i ].getP2().getW()[ 0 ];
+				ry[ i ] = ( int )pq[ i ].getP2().getW()[ 1 ];
+			}
+				
+			rx[ targetIndex ] = x;
+			ry[ targetIndex ] = y;
+			
+			handles = new PointRoi( rx, ry, pq.length );
+			imp.setRoi( handles );
+				
+			fq[ 0 ] = x;
+			fq[ 1 ] = y;
+			
+			mt.update( pq[ targetIndex ] );
+					
+			mt.apply( ipOrig, ip );
+		}
+	}
+	
+	public void mouseMoved( MouseEvent e ){}
+	
+	
+	public static String modifiers( int flags )
+	{
+		String s = " [ ";
+		if ( flags == 0 )
+			return "";
+		if ( ( flags & Event.SHIFT_MASK ) != 0 )
+			s += "Shift ";
+		if ( ( flags & Event.CTRL_MASK ) != 0 )
+			s += "Control ";
+		if ( ( flags & Event.META_MASK ) != 0 )
+			s += "Meta (right button) ";
+		if ( ( flags & Event.ALT_MASK ) != 0 )
+			s += "Alt ";
+		s += "]";
+		if ( s.equals( " [ ]" ) )
+			s = " [no modifiers]";
+		return s;
+	}
+}
