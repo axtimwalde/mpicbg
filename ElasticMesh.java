80448a7b2be11566f3cfdb8229165cc5a2a45bb6
Stephan <saalfeld@mpi-cbg.de>
2008-06-18 13:46:27 +0200

    - changed the tile model for the vertices, tiles share the initila image coordinate system   now, that is Points may be attached using their original coordinates---byt his means   Points may be shared between tiles (important for weighted influence) - TODO revise the interconnection of vertices

--- ElasticMesh.java
+++ ElasticMesh.java
@@ -40,40 +40,28 @@ public class ElasticMesh extends TransformMesh
 	final static private DecimalFormat decimalFormat = new DecimalFormat();
 	final static private DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
 	
-	public ElasticMesh()
+	public ElasticMesh( int numX, int numY, float width, float height )
 	{
+		super( numX, numY, width, height );
+		
 		decimalFormatSymbols.setGroupingSeparator( ',' );
 		decimalFormatSymbols.setDecimalSeparator( '.' );
 		decimalFormat.setDecimalFormatSymbols( decimalFormatSymbols );
 		decimalFormat.setMaximumFractionDigits( 3 );
-		decimalFormat.setMinimumFractionDigits( 3 );		
-	}
-	
-	
-	public void init()
-	{
-		/**
-		 * Cleanup
-		 */
-		pt.clear();
-		fixedTiles.clear();
+		decimalFormat.setMinimumFractionDigits( 3 );
 		
 		Set< PointMatch > s = l.keySet();
-		HashMap< PointMatch, Point > points = new HashMap< PointMatch, Point >();
+		//HashMap< PointMatch, Point > points = new HashMap< PointMatch, Point >();
 		for ( PointMatch handle : s )
 		{
 			/**
-			 * Instantiate a RigidModel with its translation being the handle's
-			 * location.
+			 * Create a tile for each handle.
 			 */
 			RigidModel2D model = new RigidModel2D();
-			float[] tr = handle.getP2().getW();
-			model.getAffine().translate( tr[ 0 ], tr[ 1 ] );
-			Tile t = new Tile( 0, 0, model );
+			Tile t = new Tile( width, height, model );
 			pt.put( handle, t );
 			Point p = new Point( new float[]{ 0, 0 } );
-			points.put(  handle, p );
-			t.update();
+			//t.update();
 		}
 		
 		for ( PointMatch handle : s )
@@ -94,23 +82,23 @@ public class ElasticMesh extends TransformMesh
 			 * Add PointMatches for each connectedHandle.
 			 */
 			Tile t = pt.get( handle );
-			float[] here = handle.getP2().getW();
+			//float[] here = handle.getP2().getW();
 			for ( PointMatch m : connectedHandles )
 			{
 				Tile o = pt.get( m );
-				Point p2 = points.get( m );
-				float[] there = m.getP2().getW();
-				Point p1 = new Point( new float[]{ there[ 0 ] - here[ 0 ], there[ 1 ] - here[ 1 ] } );
+				Point p2 = m.getP2();
+				Point p1 = new Point( p2.getW().clone() );
 				
 				//System.out.println( "Adding virtual intra-section PointMatch at: (" + p1.getL()[ 0 ] + ", " + p1.getL()[ 1 ] );
 				
-				p1.apply( pt.get( m ).getModel() );
+				//p1.apply( pt.get( m ).getModel() );
+				//t.addMatch( new PointMatch( p1, p2 ) );
 				t.addMatch( new PointMatch( p1, p2 ) );
 				t.addConnectedTile( o );
-				t.update();
-				o.addMatch( new PointMatch( p2, p1 ) );
-				o.addConnectedTile( t );
-				o.update();
+				//t.update();
+				//o.addMatch( new PointMatch( p2, p1 ) );
+				//o.addConnectedTile( t );
+				//o.update();
 				
 			}
 		}
@@ -123,9 +111,10 @@ public class ElasticMesh extends TransformMesh
 	 * 
 	 * If you want to add a PointMatch between two Tiles t and o do the
 	 * following.
-	 * Tile t is a tile in pt, Tile o is whatever you want.  The PointMatch
-	 * has p1 in Tile t and p2 in Tile o.  To find Tile t that is closest to
-	 * some world coordinate float[] (x,y) search pt with findClosest(float[]).
+	 * Tile t is a tile in pt, Tile o is whatever you want it to be.  The
+	 * PointMatch has p1 in Tile t and p2 in Tile o.  To find Tile t being
+	 * closest to some world coordinate float[] (x,y) search pt with
+	 * findClosest(float[]).
 	 * 
 	 * Then say t.addMatch(PointMatch) and o.add(PointMatch.flip())
 	 */
@@ -176,6 +165,30 @@ public class ElasticMesh extends TransformMesh
 	}
 	
 	/**
+	 * Update all PointMatches in all tiles and estimate the average
+	 * displacement by weight of the PointMatch. 
+	 */
+	final private void updateByWeight()
+	{
+		Set< PointMatch > s = l.keySet();
+		double cd = 0.0;
+		double min_d = Double.MAX_VALUE;
+		double max_d = Double.MIN_VALUE;
+		for ( PointMatch m : s )
+		{
+			Tile t = pt.get( m );
+			t.updateWeighted();
+
+			double d = t.getDistance();
+			if ( d < min_d ) min_d = d;
+			if ( d > max_d ) max_d = d;
+			cd += d;
+		}
+		cd /= pt.size();
+		error = cd;
+	}
+	
+	/**
 	 * Performs one optimization iteration and writes its error into the ErrorStatistics
 	 * 
 	 * @param observer collecting the error after update
@@ -192,24 +205,52 @@ public class ElasticMesh extends TransformMesh
 			
 			//System.out.println( t.getMatches().size() );
 			if ( fixedTiles.contains( t ) ) continue;
-			t.update();
-			t.updateModel();
+			t.fitModel();
 			t.update();
 			
 			/**
 			 * Update the location of the handle
 			 */
-			float[] w = m.getP2().getW();
-			w[ 0 ] = 0;
-			w[ 1 ] = 0;
-			t.getModel().applyInPlace( w );
+			m.getP2().apply( t.getModel() );
 			
 			error += t.getDistance();
-			update( m );
+			updateAffine( m );
 		}
 		error /= s.size();
 		
-		update();
+		observer.add( error );
+	}
+	
+	/**
+	 * Performs one optimization iteration and writes its error into the ErrorStatistics
+	 * 
+	 * @param observer collecting the error after update
+	 * @throws NotEnoughDataPointsException
+	 */
+	void optimizeIterationByWeight( ErrorStatistic observer ) throws NotEnoughDataPointsException
+	{
+		Set< PointMatch > s = l.keySet();
+		
+		error = 0.0;
+		for ( PointMatch m : s )
+		{
+			Tile t = pt.get( m );
+			
+			if ( fixedTiles.contains( t ) ) continue;
+			
+			t.fitModel();
+			t.updateWeighted();
+			
+			/**
+			 * Update the location of the handle
+			 */
+			m.getP2().apply( t.getModel() );
+			
+			error += t.getDistance();
+			updateAffine( m );
+		}
+		error /= s.size();
+		
 		observer.add( error );
 	}
 	
@@ -246,7 +287,7 @@ public class ElasticMesh extends TransformMesh
 		{
 			optimizeIteration( observer );			
 			
-			if ( i >= maxPlateauwidth && error < maxError && observer.getWideSlope( maxPlateauwidth ) >= -0.0001 )
+			if ( i >= maxPlateauwidth && error < maxError && observer.getWideSlope( maxPlateauwidth ) >= 0.0 )
 			{
 				System.out.println( "Exiting at iteration " + i + " with error " + decimalFormat.format( observer.mean ) );
 				break;
@@ -264,20 +305,46 @@ public class ElasticMesh extends TransformMesh
 		System.out.println( "  maximal displacement: " + decimalFormat.format( observer.max ) + "px" );
 	}
 	
-	public void updateMesh()
+	/**
+	 * Minimize the displacement of all PointMatches of all tiles.
+	 * 
+	 * @param maxError do not accept convergence if error is > max_error
+	 * @param maxIterations stop after that many iterations even if there was
+	 *   no minimum found
+	 * @param maxPlateauwidth convergence is reached if the average slope in
+	 *   an interval of this size is 0.0 (in double accuracy).  This prevents
+	 *   the algorithm from stopping at plateaus smaller than this value.
+	 * 
+	 * TODO  Johannes Schindelin suggested to start from a good guess, which is
+	 *   e.g. the propagated unoptimized pose of a tile relative to its
+	 *   connected tile that was already identified during RANSAC
+	 *   correspondence check.  Thank you, Johannes, great hint!
+	 */
+	public void optimizeByWeight(
+			float maxError,
+			int maxIterations,
+			int maxPlateauwidth ) throws NotEnoughDataPointsException 
 	{
-		Set< AffineModel2D > s = a.keySet();
-		for ( AffineModel2D ai : s )
+		ErrorStatistic observer = new ErrorStatistic();
+		
+		int i = 0;
+		
+		while ( i < maxIterations )  // do not run forever
 		{
-			try
+			optimizeIterationByWeight( observer );			
+			
+			if ( i >= maxPlateauwidth && error < maxError && observer.getWideSlope( maxPlateauwidth ) >= 0.0 )
 			{
-				ai.fit( a.get( ai ) );
+				System.out.println( "Exiting at iteration " + i + " with error " + decimalFormat.format( observer.mean ) );
+				break;
 			}
-			catch ( NotEnoughDataPointsException e )
-			{
-				IJ.error( e.getMessage() );
-				e.printStackTrace( System.err );
-			}
+			
+			++i;
 		}
+		
+		System.out.println( "Successfully optimized configuration of " + pt.size() + " vertices:" );
+		System.out.println( "  average displacement: " + decimalFormat.format( observer.mean ) + "px" );
+		System.out.println( "  minimal displacement: " + decimalFormat.format( observer.min ) + "px" );
+		System.out.println( "  maximal displacement: " + decimalFormat.format( observer.max ) + "px" );
 	}
 }
