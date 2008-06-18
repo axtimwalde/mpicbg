1699960f349ea3dcaa6ced34974885d03cc677b8
Stephan <saalfeld@mpi-cbg.de>
2008-06-18 13:27:41 +0200

    - PoimtMatches support an array of weights instead one weight value now - getWeight gives the product of all weights - legacy constructors create a one element weight array

--- mpicbg/models/PointMatch.java
+++ mpicbg/models/PointMatch.java
@@ -11,13 +11,65 @@ public class PointMatch
 	final private Point p2;
 	final public Point getP2() { return p2; }
 	
-	private float weight;
-	final public float getWeight(){ return weight; } 
-	final public void setWeight( float weight ){ this.weight = weight; }
+	protected float[] weights;
+	final public float[] getWeights(){ return weights; }
+	final public void setWeights( float[] weights )
+	{
+		this.weights = weights.clone();
+		calculateWeight();
+	}
+	
+	protected float weight;
+	final public float getWeight(){ return weight; }
+	final public void setWeight( int index, float weight )
+	{
+		weights[ index ] = weight;
+		calculateWeight();
+	}
+	
+	final protected void calculateWeight()
+	{
+		weight = 1.0f;
+		for ( float wi : weights )
+			weight *= wi;
+	}
 	
 	private float distance;
 	final public float getDistance(){ return distance; }
 	
+	/**
+	 * Constructor
+	 * 
+	 * Create a PointMatch with an Array of weights.
+	 * The Array of weights will be copied.
+	 * 
+	 * @param p1 Point 1
+	 * @param p2 Point 2
+	 * @param weights Array of weights
+	 */
+	public PointMatch(
+			Point p1,
+			Point p2,
+			float[] weights )
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+		
+		this.weights = weights.clone();
+		calculateWeight();
+		
+		distance = Point.distance( p1, p2 );
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * Create a PointMatch with one weight.
+	 * 
+	 * @param p1 Point 1
+	 * @param p2 Point 2
+	 * @param weight Weight
+	 */
 	public PointMatch(
 			Point p1,
 			Point p2,
@@ -26,11 +78,21 @@ public class PointMatch
 		this.p1 = p1;
 		this.p2 = p2;
 		
+		weights = new float[]{ weight };
 		this.weight = weight;
 		
 		distance = Point.distance( p1, p2 );
 	}
 	
+	/**
+	 * Constructor
+	 * 
+	 * Create a PointMatch without weight.
+	 * 
+	 * @param p1 Point 1
+	 * @param p2 Point 2
+	 * @param weights Weight
+	 */
 	public PointMatch(
 			Point p1,
 			Point p2 )
@@ -44,7 +106,7 @@ public class PointMatch
 	}
 	
 	/**
-	 * apply a model to p1, update distance
+	 * Apply a model to Point 1, update distance.
 	 * 
 	 * @param model
 	 */
@@ -55,7 +117,21 @@ public class PointMatch
 	}
 	
 	/**
-	 * flip symmetrically, weight remains unchanged
+	 * Weigthed application a model to Point 1, update distance.
+	 * Point 1 assures weight to be in the range [0,1].
+	 * That is, the resulting location ends up somewhere between its actual
+	 * location and the transferred one.
+	 * 
+	 * @param model
+	 */
+	final public void applyWeighted( Model model )
+	{
+		p1.applyWeighted( model, weight );
+		distance = Point.distance( p1, p2 );
+	}
+	
+	/**
+	 * Flip symmetrically, weights remains unchanged.
 	 * 
 	 * @param matches
 	 * @return
@@ -69,7 +145,7 @@ public class PointMatch
 					new PointMatch(
 							match.p2,
 							match.p1,
-							match.weight ) );
+							match.weights ) );
 		}
 		return list;
 	}
