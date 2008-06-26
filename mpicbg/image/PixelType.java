083e0c3280a6baa63c56f118630ac58cb16a007a
Stephan Preibisch <preibisch@mpi-cbg.de>
2008-06-26 14:03:31 +0200

    Added get methods for visible range of image

--- mpicbg/image/PixelType.java
+++ mpicbg/image/PixelType.java
@@ -15,13 +15,25 @@ interface PixelType
 	public int getNumChannels();
 	
 	/**
-	 * 
+	 * Sets the min and max for returning displayable values
 	 * @param min
 	 * @param max
 	 */
 	public void setVisibleRange( double min, double max );
 	public void setVisibleRange( double[] min, double[] max );
-	
+
+	/**
+	 * Gets the current setting for the min value for each channel
+	 * when creating displayable values
+	 */
+	public double[] getMinVisibleRange( );
+
+	/**
+	 * Gets the current setting for the max value for each channel 
+	 * when creating displayable values
+	 */
+	public double[] getMaxVisibleRange( );
+
 	/**
 	 * Computes the projection of a pixel instance to ARGB.
 	 * 
