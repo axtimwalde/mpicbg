eae94a5cc24e1fdf47704ace368bcf2d8e0a849d
Stephan <saalfeld@mpi-cbg.de>
2008-07-08 16:19:04 +0200

    ElasticMeshStack works with ElasticMovingLeastSquaresMesh now but is still in a very experimental stage...

--- ElasticMeshStack.java
+++ ElasticMeshStack.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 import mpicbg.models.ErrorStatistic;
 import mpicbg.models.NotEnoughDataPointsException;
+import mpicbg.models.PointMatch;
 
 /**
  * @author saalfeld
@@ -19,10 +20,10 @@ public class ElasticMeshStack
 	private double error = Double.MAX_VALUE;
 	public double getError(){ return error; }
 	
-	final public ArrayList< ElasticMesh > meshes = new ArrayList< ElasticMesh >();
+	final public ArrayList< ElasticMovingLeastSquaresMesh > meshes = new ArrayList< ElasticMovingLeastSquaresMesh >();
 	
-	final static private DecimalFormat decimalFormat = new DecimalFormat();
-	final static private DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
+	final static public DecimalFormat decimalFormat = new DecimalFormat();
+	final static public DecimalFormatSymbols decimalFormatSymbols = new DecimalFormatSymbols();
 	
 	public ElasticMeshStack()
 	{
@@ -34,45 +35,38 @@ public class ElasticMeshStack
 	}
 	
 	
-	public void addMesh( ElasticMesh mesh )
+	public void addMesh( ElasticMovingLeastSquaresMesh mesh )
 	{
 		meshes.add( mesh );
 	}
 	
-	/**
-	 * Performs one optimization iteration and writes its error into the ErrorStatistics
-	 * 
-	 * @param observer collecting the error after update
-	 * @throws NotEnoughDataPointsException
-	 */
-	public void optimizeIteration( ErrorStatistic observer ) throws NotEnoughDataPointsException
+	public void update( float amount )
 	{
-		error = 0.0;
-		for ( ElasticMesh m : meshes )
+		double cd = 0.0;
+		for ( ElasticMovingLeastSquaresMesh m : meshes )
 		{
-			m.optimizeIteration( observer );
-			error += m.getError();
+			m.updateByStrength( amount );
+			cd += m.getError();
 		}
-		error /= meshes.size();
-		observer.add( error );
+		cd /= meshes.size();
+		error = cd;
+	}
+	
+	public void updateAffines()
+	{
+		for ( ElasticMovingLeastSquaresMesh m : meshes )
+			m.updateAffines();
 	}
 	
 	/**
 	 * Performs one optimization iteration and writes its error into the ErrorStatistics
 	 * 
-	 * @param observer collecting the error after update
 	 * @throws NotEnoughDataPointsException
 	 */
-	public void optimizeIterationByStrength( ErrorStatistic observer ) throws NotEnoughDataPointsException
+	public void optimizeIteration() throws NotEnoughDataPointsException
 	{
-		error = 0.0;
-		for ( ElasticMesh m : meshes )
-		{
-			m.optimizeIterationByStrength( observer );
-			error += m.getError();
-		}
-		error /= meshes.size();
-		observer.add( error );
+		for ( ElasticMovingLeastSquaresMesh m : meshes )
+			m.optimizeIteration();
 	}
 	
 	/**
@@ -93,70 +87,58 @@ public class ElasticMeshStack
 	public void optimize(
 			float maxError,
 			int maxIterations,
-			int maxPlateauwidth ) throws NotEnoughDataPointsException 
+			int maxPlateauwidth,
+			ImagePlus imp,
+			ImageStack src,
+			ImageStack trg ) throws NotEnoughDataPointsException 
 	{
 		ErrorStatistic observer = new ErrorStatistic();
 		int i = 0;
 		
 		while ( i < maxIterations )  // do not run forever
 		{
-			optimizeIteration( observer );
+			optimizeIteration();
+			update( 1f );
+			observer.add( error );
 			
-			if ( i >= maxPlateauwidth && error < maxError && observer.getWideSlope( maxPlateauwidth ) >= -0.0001 )
+			paint( src, trg );
+			imp.updateAndDraw();
+			
+			double wideSlope = 0;
+			
+			if ( i >= maxPlateauwidth )
 			{
-				IJ.log( "Exiting at iteration " + i + " with error " + decimalFormat.format( observer.mean ) );
-				break;
+				wideSlope = observer.getWideSlope( maxPlateauwidth );
+				if ( i >= maxPlateauwidth && error < maxError && Math.abs( wideSlope ) <= 0.001 )
+					break;
 			}
 			
-			++i;
-		}
-		
-		System.out.println( "Successfully optimized " + meshes.size() + " slices:" );
-		System.out.println( "  average displacement: " + decimalFormat.format( observer.mean ) + "px" );
-		System.out.println( "  minimal displacement: " + decimalFormat.format( observer.min ) + "px" );
-		System.out.println( "  maximal displacement: " + decimalFormat.format( observer.max ) + "px" );
-	}
-	
-	/**
-	 * Minimize the displacement of all PointMatches of all tiles.
-	 * 
-	 * @param maxError do not accept convergence if error is > max_error
-	 * @param maxIterations stop after that many iterations even if there was
-	 *   no minimum found
-	 * @param maxPlateauwidth convergence is reached if the average slope in
-	 *   an interval of this size is 0.0 (in double accuracy).  This prevents
-	 *   the algorithm from stopping at plateaus smaller than this value.
-	 * 
-	 * TODO  Johannes Schindelin suggested to start from a good guess, which is
-	 *   e.g. the propagated unoptimized pose of a tile relative to its
-	 *   connected tile that was already identified during RANSAC
-	 *   correspondence check.  Thank you, Johannes, great hint!
-	 */
-	public void optimizeByStrength(
-			float maxError,
-			int maxIterations,
-			int maxPlateauwidth ) throws NotEnoughDataPointsException 
-	{
-		ErrorStatistic observer = new ErrorStatistic();
-		int i = 0;
-		
-		while ( i < maxIterations )  // do not run forever
-		{
-			optimizeIterationByStrength( observer );
-			
-			IJ.showStatus( "Optimizing... " + i + ": " + decimalFormat.format( error ) );
-			
-			if ( i >= maxPlateauwidth && error < maxError && Math.abs( observer.getWideSlope( maxPlateauwidth ) ) <= 0.0001 )
-				break;
+			IJ.showStatus( "Optimizing... " + i + ": " + decimalFormat.format( error ) + " ' " + decimalFormat.format( wideSlope ) );
 			
 			++i;
 		}
+		
+		updateAffines();
 		
 		IJ.log( "Exiting at iteration " + i + " with error " + decimalFormat.format( error ) );
 		System.out.println( "Successfully optimized " + meshes.size() + " slices:" );
 		System.out.println( "  average displacement: " + decimalFormat.format( observer.mean ) + "px" );
 		System.out.println( "  minimal displacement: " + decimalFormat.format( observer.min ) + "px" );
 		System.out.println( "  maximal displacement: " + decimalFormat.format( observer.max ) + "px" );
+	}
+	
+	public void paint( ImageStack src, ImageStack trg )
+	{
+		for ( int i = 0; i < meshes.size(); ++i )
+		{
+			ElasticMovingLeastSquaresMesh mesh = meshes.get( i );
+			ImageProcessor ipSrc = src.getProcessor( i + 1 );
+			ImageProcessor ipTrg = trg.getProcessor( i + 1 );
+			
+			mesh.updateAffines();
+			
+			mesh.apply( ipSrc, ipTrg );
+		}
 	}
 	
 	public void clear()
