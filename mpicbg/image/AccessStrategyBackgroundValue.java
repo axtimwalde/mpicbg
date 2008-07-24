2b80890043927e0919580170d4dc551aa3301642
Stephan <Stephan@mobilejimmy.(none)>
2008-07-24 20:54:03 +0200

    Exemplary implementation of BackgroundValue Access

--- mpicbg/image/AccessStrategyBackgroundValue.java
+++ mpicbg/image/AccessStrategyBackgroundValue.java
@@ -3,43 +3,25 @@ package mpicbg.image;
 public class AccessStrategyBackgroundValue extends AccessStrategyAbstract
 {
 	final ConstantCursor backgroundValueCursor; 
-	final Access directAccessStrategy;
 	
-	public AccessStrategyBackgroundValue(final Container container, final ConstantCursor backgroundValueCursor, final OldCursor cursor)
+	public AccessStrategyBackgroundValue(final ConstantCursor backgroundValueCursor)
 	{
-		super(container, (OldCursor)cursor);
 		this.backgroundValueCursor = backgroundValueCursor;
-		directAccessStrategy = container.createDirectAccessStrategy();
+	}	
 		
-		if (cursor != null)
-			update();
-	}	
-	
-	public AccessStrategyBackgroundValue(final Container container, final ConstantCursor backgroundValueCursor)
-	{
-		this(container, backgroundValueCursor, null);
-	}
-
 	@Override
-	public Access clone(final OldCursor c)
-	{
-		return new AccessStrategyBackgroundValue(container, backgroundValueCursor, cursor);
-	}	
-	
-	@Override
-	protected void update()
+	protected void update(final Cursor cursor)
 	{
 		if (cursor.isInside())
-		{
-			read = directAccessStrategy;
-			write = directAccessStrategy;
-			operator = directAccessStrategy;
+		{			
+			read = cursor;
+			write = cursor;
 		}
 		else
 		{
 			read = backgroundValueCursor;
-			write = backgroundValueCursor;
-			operator = backgroundValueCursor;
+			write = null;
 		}
 	}
+
 }
