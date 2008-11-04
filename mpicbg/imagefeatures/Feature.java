78617250a0e42ef10eeac3ebd4ce9033cff11620
Stephan <saalfeld@mpi-cbg.de>
2008-11-04 11:19:59 +0100

    More progress in simplifying the interface to feature extraction... TODO    This is NOT a working state

--- mpicbg/imagefeatures/Feature.java
+++ mpicbg/imagefeatures/Feature.java
@@ -2,8 +2,11 @@ package mpicbg.imagefeatures;
 
 
 import java.io.Serializable;
+import java.util.List;
 
 import mpicbg.models.InverseCoordinateTransform;
+import mpicbg.models.Point;
+import mpicbg.models.PointMatch;
 
 /**
  * Local image feature
@@ -12,12 +15,12 @@ import mpicbg.models.InverseCoordinateTransform;
  *   descriptors by a {@link InverseCoordinateTransform}.  Think about by
  *   hwich means to compare then!
  * 
- * @version 0.3b
+ * @version 0.4b
  */
 public class Feature implements Comparable< Feature >, Serializable
 {
 	/**
-	 * v0.3b
+	 * v0.4b
 	 */
 	private static final long serialVersionUID = -6425872941323995891L;
 	
@@ -44,12 +47,13 @@ public class Feature implements Comparable< Feature >, Serializable
 	 * {@link #scale this.scale} &gt; {@link #scale o.scale} to sort the
 	 * features in a <em>descending</em> order.
 	 */
-	public int compareTo( final Feature f )
+	//@Override
+	final public int compareTo( final Feature f )
 	{
 		return scale < f.scale ? 1 : scale == f.scale ? 0 : -1;
 	}
 	
-	public float descriptorDistance( final Feature f )
+	final public float descriptorDistance( final Feature f )
 	{
 		float d = 0;
 		for ( int i = 0; i < descriptor.length; ++i )
@@ -58,6 +62,72 @@ public class Feature implements Comparable< Feature >, Serializable
 			d += a * a;
 		}
 		return ( float )Math.sqrt( d );
+	}
+	
+	/**
+	 * Identify corresponding features
+	 * 
+	 * @param fs1 feature collection from set 1
+	 * @param fs2 feature collection from set 2
+	 * @param rod Ratio of distances (closest/next closest match)
+	 * 
+	 * @return matches
+	 */
+	final static public int matchFeatures(
+			final List< Feature > fs1,
+			final List< Feature > fs2,
+			final List< PointMatch > matches,
+			final float rod )
+	{
+		for ( final Feature f1 : fs1 )
+		{
+			Feature best = null;
+			float best_d = Float.MAX_VALUE;
+			float second_best_d = Float.MAX_VALUE;
+			
+			for ( final Feature f2 : fs2 )
+			{
+				float d = f1.descriptorDistance( f2 );
+				if ( d < best_d )
+				{
+					second_best_d = best_d;
+					best_d = d;
+					best = f2;
+				}
+				else if ( d < second_best_d )
+					second_best_d = d;
+			}
+			if ( best != null && second_best_d < Float.MAX_VALUE && best_d / second_best_d < rod )
+				matches.add(
+						new PointMatch(
+								new Point(
+										new float[] { f1.location[ 0 ], f1.location[ 1 ] } ),
+								new Point(
+										new float[] { best.location[ 0 ], best.location[ 1 ] } ) ) );
+		}
+		
+		// now remove ambiguous matches
+		for ( int i = 0; i < matches.size(); )
+		{
+			boolean amb = false;
+			final PointMatch m = matches.get( i );
+			final float[] m_p2 = m.getP2().getL(); 
+			for ( int j = i + 1; j < matches.size(); )
+			{
+				final PointMatch n = matches.get( j );
+				final float[] n_p2 = n.getP2().getL(); 
+				if ( m_p2[ 0 ] == n_p2[ 0 ] && m_p2[ 1 ] == n_p2[ 1 ] )
+				{
+					amb = true;
+					matches.remove( j );
+				}
+				else ++j;
+			}
+			if ( amb )
+				matches.remove( i );
+			else ++i;
+		}
+		return matches.size();
 	}
 }
 
--- mpicbg/imagefeatures/FloatArray2DFeatureTransform.java
+++ mpicbg/imagefeatures/FloatArray2DFeatureTransform.java
@@ -46,7 +46,7 @@ abstract public class FloatArray2DFeatureTransform< P >
 	
 	
 	/**
-	 * initialize the scale space as a scale pyramid having octave stubs only
+	 * Initialize the feature transform
 	 * 
 	 * @param src image having a generating gaussian kernel of initial_sigma
 	 * 	 img must be a 2d-array of float values in range [0.0f, ..., 1.0f]
@@ -61,70 +61,4 @@ abstract public class FloatArray2DFeatureTransform< P >
 	 * @return number of detected features
 	 */
 	abstract public int extractFeatures( final List< Feature > features );
-	
-	/**
-	 * Identify corresponding features
-	 * 
-	 * @param fs1 feature collection from set 1
-	 * @param fs2 feature collection from set 2
-	 * @param rod Ratio of distances (closest/next closest match)
-	 * 
-	 * @return matches
-	 */
-	static public int matchFeatures(
-			final List< Feature > fs1,
-			final List< Feature > fs2,
-			final List< PointMatch > matches,
-			final float rod )
-	{
-		for ( final Feature f1 : fs1 )
-		{
-			Feature best = null;
-			float best_d = Float.MAX_VALUE;
-			float second_best_d = Float.MAX_VALUE;
-			
-			for ( final Feature f2 : fs2 )
-			{
-				float d = f1.descriptorDistance( f2 );
-				if ( d < best_d )
-				{
-					second_best_d = best_d;
-					best_d = d;
-					best = f2;
-				}
-				else if ( d < second_best_d )
-					second_best_d = d;
-			}
-			if ( best != null && second_best_d < Float.MAX_VALUE && best_d / second_best_d < rod )
-				matches.add(
-						new PointMatch(
-								new Point(
-										new float[] { f1.location[ 0 ], f1.location[ 1 ] } ),
-								new Point(
-										new float[] { best.location[ 0 ], best.location[ 1 ] } ) ) );
-		}
-		
-		// now remove ambiguous matches
-		for ( int i = 0; i < matches.size(); )
-		{
-			boolean amb = false;
-			final PointMatch m = matches.get( i );
-			final float[] m_p2 = m.getP2().getL(); 
-			for ( int j = i + 1; j < matches.size(); )
-			{
-				final PointMatch n = matches.get( j );
-				final float[] n_p2 = n.getP2().getL(); 
-				if ( m_p2[ 0 ] == n_p2[ 0 ] && m_p2[ 1 ] == n_p2[ 1 ] )
-				{
-					amb = true;
-					matches.remove( j );
-				}
-				else ++j;
-			}
-			if ( amb )
-				matches.remove( i );
-			else ++i;
-		}
-		return matches.size();
-	}
 }
