3858b27bf9afe20937dc06d5ef7ba05a5e8e5bae
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-01-14 02:27:10 +0100

    MappingThread now paints more continuous and the cursor is set to "waiting" while painting.

--- mpicbg/ij/MappingThread.java
+++ mpicbg/ij/MappingThread.java
@@ -19,6 +19,8 @@
  */
 package mpicbg.ij;
 
+import java.awt.Canvas;
+import java.awt.Cursor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import ij.ImagePlus;
@@ -29,6 +31,7 @@ public class MappingThread extends Thread
 	final protected ImagePlus imp;
 	final protected ImageProcessor source;
 	final protected ImageProcessor target;
+	final protected ImageProcessor temp;
 	final protected AtomicBoolean pleaseRepaint;
 	final protected Mapping mapping;
 	final protected boolean interpolate;
@@ -44,6 +47,7 @@ public class MappingThread extends Thread
 		this.imp = imp;
 		this.source = source;
 		this.target = target;
+		this.temp = target.createProcessor( target.getWidth(), target.getHeight() );
 		this.pleaseRepaint = pleaseRepaint;
 		this.mapping = mapping;
 		this.interpolate = interpolate;
@@ -55,20 +59,35 @@ public class MappingThread extends Thread
 	{
 		while ( !isInterrupted() )
 		{
+			final Canvas canvas = imp.getCanvas();
+			final Cursor cursor = canvas == null ? canvas.getCursor() : Cursor.getDefaultCursor();
 			try
 			{
 				if ( pleaseRepaint.compareAndSet( true, false ) )
 				{
+					if ( canvas != null )
+						canvas.setCursor( Cursor.getPredefinedCursor( Cursor.WAIT_CURSOR ) );
 					if ( interpolate )
-						mapping.mapInterpolated( source, target );
+						mapping.mapInterpolated( source, temp );
 					else
-						mapping.map( source, target );
-					imp.updateAndDraw();
+						mapping.map( source, temp );
+					if ( !pleaseRepaint.get() )
+					{
+						final Object targetPixels = target.getPixels();
+						target.setPixels( temp.getPixels() );
+						temp.setPixels( targetPixels );
+						imp.updateAndDraw();
+					}
 				}
 				else
 					synchronized ( this ){ wait(); }
 			}
-			catch ( InterruptedException e){ Thread.currentThread().interrupt(); }
+			catch ( InterruptedException e ){ Thread.currentThread().interrupt(); }
+			finally
+			{
+				if ( canvas != null )
+					canvas.setCursor( cursor );				
+			}
 		}
 	}
 }
