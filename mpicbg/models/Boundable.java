875ed0cb6e9180d7e0ec053b3bba692de527ed77
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-04-27 18:30:48 +0200

    Added LICENSE file.  I would be happy to see all the GPL coments disappear from single source files in the future.
    Added a Boundable interface to the models package.  This interface is thought
    to be a tool for estimating the target interval given a source interval for a
    CoordinateTransform that is a continuous function or can be approximated as a
    continuous function.  In other words: find the bounding box for a given source
    box.

---
+++ mpicbg/models/Boundable.java
@@ -0,0 +1,36 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package mpicbg.models;
+
+/**
+ * A {@link CoordinateTransform} that, for a given source interval in
+ * <i>n</i>-space, can estimate the target interval in <i>n</i>-space.
+ * 
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ * @version 0.1b
+ */
+public interface Boundable extends CoordinateTransform
+{
+	/**
+	 * Estimate the bounds of an n-dimensional interval [min,max] with min and
+	 * max being n-dimensional vectors.
+	 * 
+	 * @param min
+	 * @param max
+	 */
+	public void estimateBounds( final float[] min, final float[] max );
+}
---
+++ mpicbg/models/InvertibleBoundable.java
@@ -0,0 +1,8 @@
+package mpicbg.models;
+
+/**
+ * 
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ * @version 0.4b
+ */
+public interface InvertibleBoundable extends Boundable, InverseBoundable {}
