2b227464ad8fb98a1f131a22da6b854c5d7d16eb
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2009-11-16 13:07:08 +0100

    * Undoable * respect ROI * public method that handles non-binary masks
    Signed-off-by: Stephan Saalfeld <saalfeld@mpi-cbg.de>

--- mpicbg/ij/CLAHE.java
+++ mpicbg/ij/CLAHE.java
@@ -16,10 +16,13 @@
  */
 package mpicbg.ij;
 
-import mpicbg.util.Util;
+import java.awt.Rectangle;
+
 import ij.IJ;
 import ij.ImagePlus;
+import ij.Undo;
 import ij.gui.GenericDialog;
+import ij.gui.Roi;
 import ij.plugin.PlugIn;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
@@ -51,12 +54,13 @@ public class CLAHE implements PlugIn
 	static private int bins = 255;
 	static private float slope = 3;
 	
-	final boolean setup()
+	final static boolean setup()
 	{
 		final GenericDialog gd = new GenericDialog( "CLAHE" );
 		gd.addNumericField( "blocksize : ", blockRadius * 2 + 1, 0 );
 		gd.addNumericField( "histogram bins : ", bins + 1, 0 );
 		gd.addNumericField( "maximum slope : ", slope, 2 );
+		gd.addHelp( "http://pacific.mpi-cbg.de/wiki/index.php/Enhance_Local_Contrast_(CLAHE)" );
 		
 		gd.showDialog();
 		
@@ -69,69 +73,182 @@ public class CLAHE implements PlugIn
 		return true;
 	}
 	
-	public void run( String arg )
+	final public void run( final String arg )
 	{
 		final ImagePlus imp = IJ.getImage();
+		synchronized ( imp )
+		{
+			if ( !imp.isLocked() )
+				imp.lock();
+			else
+			{
+				IJ.error( "The image '" + imp.getTitle() + "' is being processed currently.\nPlease wait until the process is done and try again." );
+				return;
+			}
+		}
 		
-		if ( !setup() ) return;
+		if ( !setup() )
+		{
+			imp.unlock();
+			return;
+		}
 		
 		run( imp );
+		imp.unlock();
 	}
-		
 	
-	public void run( final ImagePlus imp )
+	/**
+	 * Process an {@link ImagePlus} with the static parameters.  Create mask
+	 * and bounding box from the {@link Roi} of that {@link ImagePlus}.
+	 * 
+	 * @param imp
+	 */
+	final static public void run( final ImagePlus imp )
 	{
+		run( imp, blockRadius, bins, slope );
+	}
+	
+	/**
+	 * Process and {@link ImagePlus} with a given set of parameters.  Create
+	 * mask and bounding box from the {@link Roi} of that {@link ImagePlus}.
+	 * 
+	 * @param imp
+	 * @param blockRadius
+	 * @param bins
+	 * @param slope
+	 */
+	final static public void run(
+			final ImagePlus imp,
+			final int blockRadius,
+			final int bins,
+			final float slope )
+	{
+		final Roi roi = imp.getRoi();
+		final ByteProcessor mask;
+		final Rectangle roiBox;
+		if ( roi == null )
+		{
+			mask = null;
+			roiBox = new Rectangle( 0, 0, imp.getWidth() - 1, imp.getHeight() - 1 );
+		}
+		else
+		{
+			roiBox = roi.getBounds();
+			final ImageProcessor m = roi.getMask();
+			if ( m != null )
+				mask = ( ByteProcessor )m.convertToByte( false );
+			else
+				mask = null;
+		}
+		
+		run( imp, blockRadius, bins, slope, roiBox, mask );
+	}
+	
+	/**
+	 * Process and {@link ImagePlus} with a given set of parameters including
+	 * the bounding box and mask.
+	 * 
+	 * @param imp
+	 * @param blockRadius
+	 * @param bins
+	 * @param slope
+	 * @param roiBox can be null
+	 * @param mask can be null
+	 */
+	final static public void run(
+			final ImagePlus imp,
+			final int blockRadius,
+			final int bins,
+			final float slope,
+			final java.awt.Rectangle roiBox,
+			final ByteProcessor mask )
+	{
+		/* initialize box if necessary */
+		final Rectangle box;
+		if ( roiBox == null )
+		{
+			if ( mask == null )
+				box = new Rectangle( 0, 0, imp.getWidth(), imp.getHeight() );
+			else
+				box = new Rectangle( 0, 0, Math.min( imp.getWidth(), mask.getWidth() ), Math.min( imp.getHeight(), mask.getHeight() ) );
+		}
+		else
+			box = roiBox;
+		
+		/* make sure that the box is not larger than the mask */
+		if ( mask != null )
+		{
+			box.width = Math.min( mask.getWidth(), box.width );
+			box.height = Math.min( mask.getHeight(), box.height );
+		}
+		
+		/* make sure that the box is not larger than the image */
+		box.width = Math.min( imp.getWidth() - box.x, box.width );
+		box.height = Math.min( imp.getHeight() - box.y, box.height );
+		
+		final int boxXMax = box.x + box.width;
+		final int boxYMax = box.y + box.height;
+		
+		/* convert 8bit processors with a LUT to RGB and create Undo-step */
 		final ImageProcessor ip;
 		if ( imp.getType() == ImagePlus.COLOR_256 )
 		{
+			Undo.setup( Undo.TYPE_CONVERSION, imp );
 			ip = imp.getProcessor().convertToRGB();
 			imp.setProcessor( imp.getTitle(), ip );
 		}
 		else
+		{
 			ip = imp.getProcessor();
-			
+			ip.snapshot();
+			Undo.setup( Undo.FILTER, imp );
+		}
+		
+		/* work on ByteProcessors that reflect the user defined intensity range */
 		final ByteProcessor src;
-		if ( imp.getType() == ImagePlus.GRAY8 || imp.getType() == ImagePlus.COLOR_256 )
+		if ( imp.getType() == ImagePlus.GRAY8 )
 			src = ( ByteProcessor )ip.convertToByte( true ).duplicate();
 		else
 			src = ( ByteProcessor )ip.convertToByte( true );
 		final ByteProcessor dst = ( ByteProcessor )src.duplicate();
 		
-		dst.snapshot();
-		
-		//imp.setProcessor( imp.getTitle(), dst );
-		for ( int y = 0; y < imp.getHeight(); ++y )
+		for ( int y = box.y; y < boxYMax; ++y )
 		{
 			final int yMin = Math.max( 0, y - blockRadius );
 			final int yMax = Math.min( imp.getHeight(), y + blockRadius + 1 );
 			final int h = yMax - yMin;
 			
-			final int xMax0 = Math.min( imp.getWidth(), blockRadius + 1 );
+			final int xMin0 = Math.max( 0, box.x - blockRadius );
+			final int xMax0 = Math.min( imp.getWidth(), box.x + blockRadius + 1 );
 			
 			/* initially fill histogram */
 			final int[] hist = new int[ bins + 1 ];
 			final int[] clippedHist = new int[ bins + 1 ];
 			for ( int yi = yMin; yi < yMax; ++yi )
-				for ( int xi = 0; xi < xMax0; ++xi )
-					++hist[ Util.round( src.get( xi, yi ) / 255.0f * bins ) ];
+				for ( int xi = xMin0; xi < xMax0; ++xi )
+					++hist[ roundPositive( src.get( xi, yi ) / 255.0f * bins ) ];
 			
-			for ( int x = 0; x < imp.getWidth(); ++x )
+			for ( int x = box.x; x < boxXMax; ++x )
 			{
-				final int v = Util.round( src.get( x, y ) / 255.0f * bins );
+				final int v = roundPositive( src.get( x, y ) / 255.0f * bins );
 				
 				final int xMin = Math.max( 0, x - blockRadius );
 				final int xMax = Math.min( imp.getWidth(), x + blockRadius + 1 );
 				final int w = xMax - xMin;
 				final int n = h * w;
 				
-				final int limit = ( int )( slope * n / bins + 0.5f );
+				final int limit;
+				if ( mask == null )
+					limit = ( int )( slope * n / bins + 0.5f );
+				else
+					limit = ( int )( ( 1 + mask.get( x - box.x,  y - box.y ) / 255.0f * ( slope - 1 ) ) * n / bins + 0.5f );
 				
 				/* remove left behind values from histogram */
 				if ( xMin > 0 )
 				{
 					final int xMin1 = xMin - 1;
 					for ( int yi = yMin; yi < yMax; ++yi )
-						--hist[ Util.round( src.get( xMin1, yi ) / 255.0f * bins ) ];						
+						--hist[ roundPositive( src.get( xMin1, yi ) / 255.0f * bins ) ];						
 				}
 					
 				/* add newly included values to histogram */
@@ -139,7 +256,7 @@ public class CLAHE implements PlugIn
 				{
 					final int xMax1 = xMax - 1;
 					for ( int yi = yMin; yi < yMax; ++yi )
-						++hist[ Util.round( src.get( xMax1, yi ) / 255.0f * bins ) ];						
+						++hist[ roundPositive( src.get( xMax1, yi ) / 255.0f * bins ) ];						
 				}
 				
 				/* clip histogram and redistribute clipped entries */
@@ -173,7 +290,6 @@ public class CLAHE implements PlugIn
 				}
 				while ( clippedEntries != clippedEntriesBefore );
 				
-				
 				/* build cdf of clipped histogram */
 				int hMin = bins;
 				for ( int i = 0; i < hMin; ++i )
@@ -189,14 +305,14 @@ public class CLAHE implements PlugIn
 				
 				final int cdfMin = clippedHist[ hMin ];
 				
-				dst.set( x, y, Util.round( ( cdf - cdfMin ) / ( float )( cdfMax - cdfMin ) * 255.0f ) );
+				dst.set( x, y, roundPositive( ( cdf - cdfMin ) / ( float )( cdfMax - cdfMin ) * 255.0f ) );
 			}
 			
 			/* multiply the current row into ip */
 			final int t = y * imp.getWidth();
 			if ( imp.getType() == ImagePlus.GRAY8 )
 			{
-				for ( int x = 0; x < imp.getWidth(); ++x )
+				for ( int x = box.x; x < imp.getWidth(); ++x )
 				{
 					final int i = t + x;
 					ip.set( i, dst.get( i ) );
@@ -210,7 +326,7 @@ public class CLAHE implements PlugIn
 					final int i = t + x;
 					final int v = ip.get( i );
 					final float a = ( float )dst.get( i ) / src.get( i );
-					ip.set( i, Math.max( 0, Math.min( 65535, Util.round( a * ( v - min ) + min ) ) ) );
+					ip.set( i, Math.max( 0, Math.min( 65535, roundPositive( a * ( v - min ) + min ) ) ) );
 				}
 			}
 			else if ( imp.getType() == ImagePlus.GRAY32 )
@@ -231,13 +347,19 @@ public class CLAHE implements PlugIn
 					final int i = t + x;
 					final int argb = ip.get( i );
 					final float a = ( float )dst.get( i ) / src.get( i );
-					final int r = Math.max( 0, Math.min( 255, Util.round( a * ( ( argb >> 16 ) & 0xff ) ) ) );  
-					final int g = Math.max( 0, Math.min( 255, Util.round( a * ( ( argb >> 8 ) & 0xff ) ) ) );
-					final int b = Math.max( 0, Math.min( 255, Util.round( a * ( argb & 0xff ) ) ) );
+					final int r = Math.max( 0, Math.min( 255, roundPositive( a * ( ( argb >> 16 ) & 0xff ) ) ) );  
+					final int g = Math.max( 0, Math.min( 255, roundPositive( a * ( ( argb >> 8 ) & 0xff ) ) ) );
+					final int b = Math.max( 0, Math.min( 255, roundPositive( a * ( argb & 0xff ) ) ) );
 					ip.set( i, ( r << 16 ) | ( g << 8 ) | b );
 				}
 			}
 			imp.updateAndDraw();
-		}	
+		}
 	}
+	
+	final static private int roundPositive( float a )
+	{
+		return ( int )( a + 0.5f );
+	}
+
 }
