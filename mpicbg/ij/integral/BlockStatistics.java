696c549e480204f622c171275ba1b0c126e463d1
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2011-03-21 15:22:06 +0100

    added BlockStatistics, a class that provides mean, std and variance for arbitrary size blocks in floating point images from two integral images, one over x and one over x^2

---
+++ mpicbg/ij/integral/BlockStatistics.java
@@ -0,0 +1,205 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package mpicbg.ij.integral;
+
+import ij.process.FloatProcessor;
+
+/**
+ * 
+ *
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ */
+final public class BlockStatistics
+{
+	final private DoubleIntegralImage sums;
+	final private DoubleIntegralImage sumsOfSquares;
+	final private FloatProcessor fp;
+	
+	public BlockStatistics( final FloatProcessor fp )
+	{
+		this.fp = fp;
+		
+		final int width = fp.getWidth();
+		final int height = fp.getHeight();
+		
+		final int w = width + 1;
+
+		final double[] sum = new double[ w * ( height + 1 ) ];
+		final double[] sumOfSquares = new double[ sum.length ];
+		
+		double s = 0;
+		double ss = 0;
+		for ( int x = 0; x < width; )
+		{
+			final float a = fp.getf( x );
+			final int i = ++x + w;
+			
+			s += a;
+			sum[ i ] = s;
+			
+			ss += a * a;
+			sumOfSquares[ i ] = ss;
+		}
+		for ( int y = 1; y < height; ++y )
+		{
+			final int ywidth = y * width;
+			final int yw = y * w + w + 1;
+			final int yww = yw - w;
+			
+			final float a = fp.getf( ywidth );
+			
+			sum[ yw ] = sum[ yww ] + a;
+			sumOfSquares[ yw ] = sumOfSquares[ yww ] + a * a;
+			for ( int x = 1; x < width; ++x )
+			{
+				final int ywx = yw + x;
+				final int ywx1 = ywx - 1;
+				final int ywxw = ywx - w;
+				final int ywxw1 = ywxw - 1;
+				
+				final float b = fp.getf( ywidth + x );
+				
+				sum[ ywx ] = b + sum[ ywxw ] + sum[ ywx1 ] - sum[ ywxw1 ];
+				sumOfSquares[ ywx ] = b * b + sumOfSquares[ ywxw ] + sumOfSquares[ ywx1 ] - sumOfSquares[ ywxw1 ];
+			}
+		}
+		
+		sums = new DoubleIntegralImage( sum, width, height );
+		sumsOfSquares = new DoubleIntegralImage( sumOfSquares, width, height );
+	}
+	
+	
+	/**
+	 * Set all pixels in <code>ip</code> to their block mean for a block
+	 * with given radius.
+	 * 
+	 * @param blockRadiusX
+	 * @param blockRadiusY
+	 */
+	final public void mean( final int blockRadiusX, final int blockRadiusY )
+	{
+		final int width = fp.getWidth();
+		final int w = fp.getWidth() - 1;
+		final int h = fp.getHeight() - 1;
+		for ( int y = 0; y <= h; ++y )
+		{
+			final int row = y * width;
+			final int yMin = Math.max( -1, y - blockRadiusY - 1 );
+			final int yMax = Math.min( h, y + blockRadiusY );
+			final int bh = yMax - yMin;
+			for ( int x = 0; x <= w; ++x )
+			{
+				final int xMin = Math.max( -1, x - blockRadiusX - 1 );
+				final int xMax = Math.min( w, x + blockRadiusX );
+				final float scale = 1.0f / ( xMax - xMin ) / bh;
+				fp.setf( row + x, ( float )( scale * sums.getDoubleSum( xMin, yMin, xMax, yMax ) ) );
+			}
+		}
+	}
+	
+	final public void mean( final int blockRadius )
+	{
+		mean( blockRadius, blockRadius );
+	}
+	
+	
+	/**
+	 * Set all pixels in <code>ip</code> to their block variance for a block
+	 * with given radius.
+	 * 
+	 * @param blockRadiusX
+	 * @param blockRadiusY
+	 */
+	final public void variance( final int blockRadiusX, final int blockRadiusY )
+	{
+		final int width = fp.getWidth();
+		final int w = fp.getWidth() - 1;
+		final int h = fp.getHeight() - 1;
+		for ( int y = 0; y <= h; ++y )
+		{
+			final int row = y * width;
+			final int yMin = Math.max( -1, y - blockRadiusY - 1 );
+			final int yMax = Math.min( h, y + blockRadiusY );
+			final int bh = yMax - yMin;
+			for ( int x = 0; x <= w; ++x )
+			{
+				final int xMin = Math.max( -1, x - blockRadiusX - 1 );
+				final int xMax = Math.min( w, x + blockRadiusX );
+				final float scale = 1.0f / ( xMax - xMin ) / bh;
+				final double sum = sums.getDoubleSum( xMin, yMin, xMax, yMax );
+				final double var = scale * ( sumsOfSquares.getDoubleSum( xMin, yMin, xMax, yMax ) - sum * sum * scale );
+				
+				fp.setf( row + x, var < 0 ? 0 : ( float )var );
+			}
+		}
+	}
+	
+	/**
+	 * Set all pixels in <code>ip</code> to their block variance for a block
+	 * with given radius.
+	 * 
+	 * @param blockRadius
+	 */
+	final public void variance( final int blockRadius )
+	{
+		variance( blockRadius, blockRadius );
+	}
+	
+	
+	/**
+	 * Set all pixels in <code>ip</code> to their block STD for a block with
+	 * given radius.
+	 * 
+	 * @param blockRadiusX
+	 * @param blockRadiusY
+	 */
+	final public void std( final int blockRadiusX, final int blockRadiusY )
+	{
+		final int width = fp.getWidth();
+		final int w = fp.getWidth() - 1;
+		final int h = fp.getHeight() - 1;
+		for ( int y = 0; y <= h; ++y )
+		{
+			final int row = y * width;
+			final int yMin = Math.max( -1, y - blockRadiusY - 1 );
+			final int yMax = Math.min( h, y + blockRadiusY );
+			final int bh = yMax - yMin;
+			for ( int x = 0; x <= w; ++x )
+			{
+				final int xMin = Math.max( -1, x - blockRadiusX - 1 );
+				final int xMax = Math.min( w, x + blockRadiusX );
+				final float scale = 1.0f / ( xMax - xMin ) / bh;
+				final double sum = sums.getDoubleSum( xMin, yMin, xMax, yMax );
+				final double var = scale * ( sumsOfSquares.getDoubleSum( xMin, yMin, xMax, yMax ) - sum * sum * scale );
+				
+				fp.setf( row + x, var < 0 ? 0 : ( float )Math.sqrt( var ) );
+			}
+		}
+	}
+	
+	/**
+	 * Set all pixels in <code>ip</code> to their block STD for a block with
+	 * given radius.
+	 * 
+	 * @param blockRadiusX
+	 * @param blockRadiusY
+	 */
+	final public void std( final int blockRadius )
+	{
+		std( blockRadius, blockRadius );
+	}
+}
---
+++ mpicbg/ij/integral/InteractiveVariance.java
@@ -0,0 +1,203 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package mpicbg.ij.integral;
+
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.gui.ImageWindow;
+import ij.gui.Roi;
+import ij.gui.Toolbar;
+import ij.plugin.PlugIn;
+import ij.process.FloatProcessor;
+import ij.process.ImageProcessor;
+
+import java.awt.Canvas;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/**
+ * 
+ *
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ */
+public class InteractiveVariance implements KeyListener, MouseListener, MouseMotionListener, PlugIn
+{
+	final static private String NL = System.getProperty( "line.separator" );
+	
+	private int blockRadiusX = 0, blockRadiusY = 0;
+	private ImageJ ij;
+	private ImagePlus imp;
+	private FloatProcessor fp;
+	private ImageProcessor ipOriginal;
+	private ImageWindow window;
+	private Canvas canvas;
+	private BlockStatistics variance;
+	private PaintThread painter;
+	
+	@Override
+	public void run( String arg )
+	{
+		ij = IJ.getInstance();
+		imp = IJ.getImage();
+		window = imp.getWindow();
+		canvas = imp.getCanvas();
+		
+		canvas.addKeyListener( this );
+		window.addKeyListener( this );
+		canvas.addMouseMotionListener( this );
+		canvas.addMouseListener( this );
+		ij.addKeyListener( this );
+		
+		ipOriginal = imp.getProcessor();
+		if ( imp.getType() == ImagePlus.GRAY32 )
+			fp = ( FloatProcessor )ipOriginal.duplicate();
+		else
+			fp = ( FloatProcessor )ipOriginal.convertToFloat();
+		
+		imp.setProcessor( fp );
+		
+		variance = new BlockStatistics( fp );
+		
+		imp.getProcessor().snapshot();
+		
+		Toolbar.getInstance().setTool( Toolbar.RECTANGLE );
+		
+		painter = new PaintThread();
+		painter.start();
+		
+	}
+	
+	final private void draw()
+	{
+		variance.variance( blockRadiusX, blockRadiusY );
+	}
+	
+	public class PaintThread extends Thread
+	{
+		private boolean pleaseRepaint;
+		
+		PaintThread()
+		{
+			this.setName( "MappingThread" );
+		}
+		
+		@Override
+		public void run()
+		{
+			while ( !isInterrupted() )
+			{
+				final boolean b;
+				synchronized ( this )
+				{
+					b = pleaseRepaint;
+					pleaseRepaint = false;
+				}
+				if ( b )
+				{
+					draw();
+					imp.updateAndDraw();
+				}
+				synchronized ( this )
+				{
+					try
+					{
+						if ( !pleaseRepaint ) wait();
+					}
+					catch ( InterruptedException e ){}
+				}
+			}
+		}
+		
+		public void repaint()
+		{
+			synchronized ( this )
+			{
+				pleaseRepaint = true;
+				notify();
+			}
+		}
+	}
+	
+	public void keyPressed( KeyEvent e )
+	{
+		if ( e.getKeyCode() == KeyEvent.VK_ESCAPE || e.getKeyCode() == KeyEvent.VK_ENTER )
+		{
+			painter.interrupt();
+			
+			canvas.removeKeyListener( this );
+			window.removeKeyListener( this );
+			ij.removeKeyListener( this );
+			canvas.removeMouseListener( this );
+			canvas.removeMouseMotionListener( this );
+			
+			if ( imp != null )
+			{
+				if ( e.getKeyCode() == KeyEvent.VK_ESCAPE )
+				{
+					imp.setProcessor( ipOriginal );
+				}
+				else if ( e.getKeyCode() == KeyEvent.VK_ENTER )
+				{
+				}
+			}
+			imp.updateAndDraw();
+		}
+		else if ( e.getKeyCode() == KeyEvent.VK_F1 )
+		{
+			IJ.showMessage(
+					"Interactive Block Variance",
+					"Click and drag to change the size of the smoothing kernel." + NL +
+					"ENTER - Apply" + NL +
+					"ESC - Cancel" );
+		}
+	}
+	
+	public void keyReleased( KeyEvent e ) {}
+	public void keyTyped( KeyEvent e ) {}
+
+	public void mouseDragged( final MouseEvent e )
+	{
+		final Roi roi = imp.getRoi();
+		if ( roi != null )
+		{
+			final Rectangle bounds = imp.getRoi().getBounds();
+			blockRadiusX = bounds.width / 2;
+			blockRadiusY = bounds.height / 2;
+		}
+		else
+		{
+			blockRadiusX = 0;
+			blockRadiusY = 0;	
+		}
+		painter.repaint();
+	}
+
+	public void mouseMoved( MouseEvent e ){}
+	public void mouseClicked( MouseEvent e ){}
+	public void mouseEntered( MouseEvent e ){}
+	public void mouseExited( MouseEvent e ){}
+	public void mouseReleased( MouseEvent e ){}
+	public void mousePressed( MouseEvent e )
+	{
+		mouseDragged( e );
+	}
+}
