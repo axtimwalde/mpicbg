ae97ddf8e2880a82f03a8a7560497371f8c11f13
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2011-04-08 19:23:16 +0200

    added constructors for equal size images to BlockPMCC

--- mpicbg/ij/integral/BlockPMCC.java
+++ mpicbg/ij/integral/BlockPMCC.java
@@ -39,7 +39,10 @@ final public class BlockPMCC
 	
 	private int fpXYWidth, fpXYHeight, offsetXX, offsetYX, offsetXY, offsetYY;
 	
-	final static private void sumAndSumOfSquares( final FloatProcessor fp, final double[] sum, final double[] sumOfSquares )
+	final static private void sumAndSumOfSquares(
+			final FloatProcessor fp,
+			final double[] sum,
+			final double[] sumOfSquares )
 	{
 		final int width = fp.getWidth();
 		final int height = fp.getHeight();
@@ -84,22 +87,145 @@ final public class BlockPMCC
 		}
 	}
 	
+	final static private void sumAndSumOfSquares(
+			final int width,
+			final int height,
+			final FloatProcessor fp1,
+			final double[] sum1,
+			final double[] sumOfSquares1,
+			final FloatProcessor fp2,
+			final double[] sum2,
+			final double[] sumOfSquares2 )
+	{
+		final int w = width + 1;
+
+		double s1 = 0;
+		double ss1 = 0;
+		double s2 = 0;
+		double ss2 = 0;
+		for ( int x = 0; x < width; )
+		{
+			final float a1 = fp1.getf( x );
+			final float a2 = fp2.getf( x );
+			final int i = ++x + w;
+			
+			s1 += a1;
+			sum1[ i ] = s1;
+			s2 += a2;
+			sum2[ i ] = s2;
+			
+			ss1 += a1 * a1;
+			sumOfSquares1[ i ] = ss1;
+			ss2 += a2 * a2;
+			sumOfSquares2[ i ] = ss2;
+		}
+		for ( int y = 1; y < height; ++y )
+		{
+			final int ywidth = y * width;
+			final int yw = y * w + w + 1;
+			final int yww = yw - w;
+			
+			final float a1 = fp1.getf( ywidth );
+			final float a2 = fp2.getf( ywidth );
+			
+			sum1[ yw ] = sum1[ yww ] + a1;
+			sumOfSquares1[ yw ] = sumOfSquares1[ yww ] + a1 * a1;
+			sum2[ yw ] = sum2[ yww ] + a2;
+			sumOfSquares2[ yw ] = sumOfSquares2[ yww ] + a2 * a2;
+			
+			for ( int x = 1; x < width; ++x )
+			{
+				final int ywx = yw + x;
+				final int ywx1 = ywx - 1;
+				final int ywxw = ywx - w;
+				final int ywxw1 = ywxw - 1;
+				
+				final float b1 = fp1.getf( ywidth + x );
+				final float b2 = fp2.getf( ywidth + x );
+				
+				sum1[ ywx ] = b1 + sum1[ ywxw ] + sum1[ ywx1 ] - sum1[ ywxw1 ];
+				sumOfSquares1[ ywx ] = b1 * b1 + sumOfSquares1[ ywxw ] + sumOfSquares1[ ywx1 ] - sumOfSquares1[ ywxw1 ];
+				sum2[ ywx ] = b2 + sum2[ ywxw ] + sum2[ ywx1 ] - sum2[ ywxw1 ];
+				sumOfSquares2[ ywx ] = b2 * b2 + sumOfSquares2[ ywxw ] + sumOfSquares2[ ywx1 ] - sumOfSquares2[ ywxw1 ];
+			}
+		}
+	}
+	
 	
 	/**
-	 * Two loops because fpX and fpY can have different dimensions.
+	 * Special case constructor for fpX and fpY having equal dimensions.
 	 * 
-	 * @TODO make a constructor for equal size fpX and fpY to save that additional loop.
+	 * <p>Note, that this constructor does not initialize &Sigma;XY.  That has
+	 * to be done for a specified offset through {@link #setOffset(int, int)}
+	 * afterwards.</p>
 	 * 
+	 * @param width
+	 * @param height
+	 * @param fpX
+	 * @param fpY
+	 */
+	public BlockPMCC(
+			final int width,
+			final int height,
+			final FloatProcessor fpX,
+			final FloatProcessor fpY )
+	{
+		this.fpX = fpX;
+		this.fpY = fpY;
+		
+		fpR = new FloatProcessor( width, height );
+		
+		final double[] sumX = new double[ ( width + 1 ) * ( height + 1 ) ];
+		final double[] sumXX = new double[ sumX.length ];
+		final double[] sumY = new double[ ( width + 1 ) * ( height + 1 ) ];
+		final double[] sumYY = new double[ sumY.length ];
+		final double[] sumXY = new double[ ( width + 1 ) * ( height + 1 ) ];
+		
+		sumAndSumOfSquares( width, height, fpX, sumX, sumXX, fpY, sumY, sumYY );
+		
+		sumsX = new DoubleIntegralImage( sumX, width, height );
+		sumsXX = new DoubleIntegralImage( sumXX, width, height );
+		sumsY = new DoubleIntegralImage( sumY, width, height );
+		sumsYY = new DoubleIntegralImage( sumYY, width, height );
+		sumsXY = new DoubleIntegralImage( sumXY, width, height );
+	}
+	
+	
+	/**
+	 * Special case constructor for fpX and fpY having equal dimensions.
+	 * 
+	 * @param width
+	 * @param height
 	 * @param fpX source (moving)
 	 * @param fpY target
 	 * @param offsetX source <em>x</em> offset relative to target
 	 * @param offsetY source <em>y</em> offset relative to target
 	 */
 	public BlockPMCC(
+			final int width,
+			final int height,
 			final FloatProcessor fpX,
 			final FloatProcessor fpY,
 			final int offsetX,
 			final int offsetY )
+	{
+		this( width, height, fpX, fpY );
+		
+		setOffset( offsetX, offsetY );
+	}
+	
+	
+	/**
+	 * Two loops because fpX and fpY can have different dimensions.
+	 * 
+	 * <p>Note, that this constructor does not initialize &Sigma;XY.  That has
+	 * to be done for a specified offset through {@link #setOffset(int, int)}
+	 * afterwards.</p>
+	 * 
+	 * @param fpX
+	 * @param fpY
+	 */
+	public BlockPMCC( final FloatProcessor fpX, final FloatProcessor fpY )
 	{
 		this.fpX = fpX;
 		this.fpY = fpY;
@@ -127,14 +253,26 @@ final public class BlockPMCC
 		sumsY = new DoubleIntegralImage( sumY, widthY, heightY );
 		sumsYY = new DoubleIntegralImage( sumYY, widthY, heightY );
 		sumsXY = new DoubleIntegralImage( sumXY, widthXY, heightXY );
-		
-		setOffset( offsetX, offsetY );
 	}
 	
 	
-	public BlockPMCC( final FloatProcessor fpX, final FloatProcessor fpY )
+	/**
+	 * Two loops because fpX and fpY can have different dimensions.
+	 * 
+	 * @param fpX source (moving)
+	 * @param fpY target
+	 * @param offsetX source <em>x</em> offset relative to target
+	 * @param offsetY source <em>y</em> offset relative to target
+	 */
+	public BlockPMCC(
+			final FloatProcessor fpX,
+			final FloatProcessor fpY,
+			final int offsetX,
+			final int offsetY )
 	{
-		this( fpX, fpY, 0, 0 );
+		this( fpX, fpY );
+		
+		setOffset( offsetX, offsetY );
 	}
 	
 	
@@ -352,5 +490,10 @@ final public class BlockPMCC
 					fpR.setf( row + x, ( float )( a * a / b ) );
 			}
 		}
+	}
+	
+	final public void rSignedSquare( final int blockRadius )
+	{
+		rSignedSquare( blockRadius, blockRadius );
 	}
 }
