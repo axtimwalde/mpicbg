7f685ad830910d276f746ce26cc840092656e1e0
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-03-01 10:18:29 -0500

    added scale space blockpmcc

--- mpicbg_/src/main/java/mpicbg/ij/plugin/PMCCBlockFlow.java
+++ mpicbg_/src/main/java/mpicbg/ij/plugin/PMCCBlockFlow.java
@@ -1,4 +1,13 @@
 package mpicbg.ij.plugin;
+import ij.CompositeImage;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.WindowManager;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+import ij.process.ColorProcessor;
+import ij.process.FloatProcessor;
 /**
  * License: GPL
  *
@@ -18,14 +27,10 @@ package mpicbg.ij.plugin;
  */
 import mpicbg.ij.integral.BlockPMCC;
 import mpicbg.ij.integral.IntegralImage;
-import ij.plugin.*;
-import ij.gui.*;
-import ij.*;
-import ij.process.*;
 
 /**
  * <h1>Transfer an image sequence into an optic flow field<h1>
- * 
+ *
  * <p>Flow fields are calculated for each pair <em>(t,t+1)</em> of the sequence
  * independently.  The motion vector for each pixel in image t is estimated by
  * searching the most similar looking pixel in image <em>t+1</em>.  The
@@ -33,20 +38,20 @@ import ij.process.*;
  * pixels in a local vicinity.  The local vicinity is defined by a block and is
  * calculated using an {@link IntegralImage}.  Both the size of the block and
  * the search radius are parameters of the method.</p>
- * 
+ *
  * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
  * @version 0.1a
- */ 
+ */
 public class PMCCBlockFlow implements PlugIn
 {
 	static protected int blockRadius = 8;
 	static protected int maxDistance = 7;
 	static protected boolean showColors = false;
-	
+
 	final static protected void colorCircle( ColorProcessor ip )
 	{
 		final int r1 = Math.min( ip.getWidth(), ip.getHeight() ) / 2;
-		
+
 		for ( int y = 0; y < ip.getHeight(); ++y )
 		{
 			final float dy = y - ip.getHeight() / 2;
@@ -54,7 +59,7 @@ public class PMCCBlockFlow implements PlugIn
 			{
 				final float dx = x - ip.getWidth() / 2;
 				final float l = ( float )Math.sqrt( dx * dx + dy * dy );
-				
+
 				if ( l > r1 )
 					ip.putPixel( x, y, 0 );
 				else
@@ -62,8 +67,8 @@ public class PMCCBlockFlow implements PlugIn
 			}
 		}
 	}
-	
-	final static private void algebraicToPolarAndColor(
+
+	final static protected void algebraicToPolarAndColor(
 			final int[] ipXPixels,
 			final int[] ipYPixels,
 			final float[] ipRPixels,
@@ -76,82 +81,82 @@ public class PMCCBlockFlow implements PlugIn
 		{
 			final double x = ipXPixels[ i ] / max;
 			final double y = ipYPixels[ i ] / max;
-			
+
 			final double r = Math.sqrt( x * x + y * y );
 			final double phi = Math.atan2( x / r, y / r );
-			
+
 			ipRPixels[ i ] = ( float )r;
 			ipPhiPixels[ i ] = ( float )phi;
-			
+
 			if ( r == 0.0 )
 				ipColorPixels[ i ] = 0;
 			else
 			{
 				final double red, green, blue;
-			
+
 				double o = ( phi + Math.PI ) / Math.PI * 3;
-				
+
 				if ( o < 3 )
 					red = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
 				else
 					red = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
-				
+
 				o += 2;
 				if ( o >= 6 ) o -= 6;
-				
+
 				if ( o < 3 )
 					green = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
 				else
 					green = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
-				
+
 				o += 2;
 				if ( o >= 6 ) o -= 6;
-				
+
 				if ( o < 3 )
 					blue = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
 				else
 					blue = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
-				
+
 				ipColorPixels[ i ] =  ( ( ( ( int )( red * 255 ) << 8 ) | ( int )( green * 255 ) ) << 8 ) | ( int )( blue * 255 );
 			}
 		}
 	}
-	
-	final static private int colorVector( float xs, float ys )
+
+	final static protected int colorVector( float xs, float ys )
 	{
 		final double a = Math.sqrt( xs * xs + ys * ys );
 		if ( a == 0.0 ) return 0;
-		
+
 		double o = ( Math.atan2( xs / a, ys / a ) + Math.PI ) / Math.PI * 3;
-		
+
 		final double r, g, b;
-		
+
 		if ( o < 3 )
 			r = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * a;
 		else
 			r = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * a;
-		
+
 		o += 2;
 		if ( o >= 6 ) o -= 6;
-		
+
 		if ( o < 3 )
 			g = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * a;
 		else
 			g = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * a;
-		
+
 		o += 2;
 		if ( o >= 6 ) o -= 6;
-		
+
 		if ( o < 3 )
 			b = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * a;
 		else
 			b = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * a;
-		
+
 		return ( ( ( ( int )( r * 255 ) << 8 ) | ( int )( g * 255 ) ) << 8 ) | ( int )( b * 255 );
 	}
-	
-	
-	final static private void opticFlow(
+
+
+	static public void opticFlow(
 			final FloatProcessor ip1,
 			final FloatProcessor ip2,
 			final FloatProcessor r,
@@ -161,37 +166,37 @@ public class PMCCBlockFlow implements PlugIn
 	{
 		final BlockPMCC bc = new BlockPMCC( ip1.getWidth(), ip1.getHeight(), ip1, ip2 );
 		//final BlockPMCC bc = new BlockPMCC( ip1, ip2 );
-		
+
 		final FloatProcessor ipR = bc.getTargetProcessor();
 		final ColorProcessor ipX = new ColorProcessor( ipR.getWidth(), ipR.getHeight() );
 		final ColorProcessor ipY = new ColorProcessor( ipR.getWidth(), ipR.getHeight() );
-		
+
 		/* init */
 		{
 			final float[] ipRMaxPixels = ( float[] )r.getPixels();
 			for ( int i = 0; i < ipRMaxPixels.length; ++i )
 				ipRMaxPixels[ i ] = -1;
 		}
-		
+
 //		final ImageStack stack = new ImageStack( ipR.getWidth(), ipR.getHeight() );
-		
+
 		for ( int yo = -maxDistance; yo <= maxDistance; ++yo )
 		{
 			for ( int xo = -maxDistance; xo <= maxDistance; ++xo )
 			{
 				// continue if radius is larger than maxDistance
 				if ( yo * yo + xo * xo > maxDistance * maxDistance ) continue;
-				
+
 				bc.setOffset( xo, yo );
 				bc.rSignedSquare( blockRadius );
-				
+
 //				stack.addSlice( xo + " " + yo, ipR.duplicate() );
-				
+
 				final float[] ipRPixels = ( float[] )ipR.getPixels();
 				final float[] ipRMaxPixels = ( float[] )r.getPixels();
 				final int[] ipXPixels = ( int[] )ipX.getPixels();
 				final int[] ipYPixels = ( int[] )ipY.getPixels();
-				
+
 				// update the translation fields
 				final int h = ipR.getHeight() - maxDistance;
 				final int width = ipR.getWidth();
@@ -212,10 +217,10 @@ public class PMCCBlockFlow implements PlugIn
 							iR = rowR + x;
 						else
 							iR = rowR + ( x - xo );
-						
+
 						final float ipRPixel = ipRPixels[ iR ];
 						final float ipRMaxPixel = ipRMaxPixels[ i ];
-						
+
 						if ( ipRPixel > ipRMaxPixel )
 						{
 							ipRMaxPixels[ i ] = ipRPixel;
@@ -227,7 +232,7 @@ public class PMCCBlockFlow implements PlugIn
 			}
 		}
 //		new ImagePlus( "rs", stack ).show();
-		
+
 		algebraicToPolarAndColor(
 				( int[] )ipX.getPixels(),
 				( int[] )ipY.getPixels(),
@@ -236,27 +241,28 @@ public class PMCCBlockFlow implements PlugIn
 				( int[] )of.getPixels(),
 				maxDistance );
 	}
-	
-	final public void run( final String args )
+
+	@Override
+	public void run( final String args )
 	{
 		if ( IJ.versionLessThan( "1.41n" ) ) return;
 
 		final ImagePlus imp = WindowManager.getCurrentImage();
 		if ( imp == null )  { IJ.error( "There are no images open" ); return; }
-		
+
 		GenericDialog gd = new GenericDialog( "Generate optic flow" );
 		gd.addNumericField( "block radius :", blockRadius, 0, 6, "px" );
 		gd.addNumericField( "maximal_distance :", maxDistance, 0, 6, "px" );
 		gd.addCheckbox( "show_color_map", showColors );
-		
+
 		gd.showDialog();
-		
+
 		if (gd.wasCanceled()) return;
-		
+
 		blockRadius = ( int )gd.getNextNumber();
 		maxDistance = ( int )gd.getNextNumber();
 		showColors = gd.getNextBoolean();
-		
+
 		if ( showColors )
 		{
 			ColorProcessor ipColor = new ColorProcessor( maxDistance * 2 + 1, maxDistance * 2 + 1 );
@@ -264,34 +270,34 @@ public class PMCCBlockFlow implements PlugIn
 			ImagePlus impColor = new ImagePlus( "Color", ipColor );
 			impColor.show();
 		}
-		
-		
+
+
 		ImageStack seq = imp.getStack();
 		ImageStack seqR = new ImageStack( imp.getWidth(), imp.getHeight(), seq.getSize() - 1 );
 		ImageStack seqOpticFlow = new ImageStack( imp.getWidth(), imp.getHeight(), seq.getSize() - 1 );
 		ImageStack seqFlowVectors = new ImageStack( imp.getWidth(), imp.getHeight(), 2 * seq.getSize() - 2 );
-		
+
 		FloatProcessor ip1;
 		FloatProcessor ip2 = ( FloatProcessor )seq.getProcessor( 1 ).convertToFloat();
-		
+
 		ImagePlus impR = null;
 		ImagePlus impOpticFlow = null;
 		CompositeImage impFlowVectors = null;
-		
+
 		for ( int i = 1; i < seq.getSize(); ++i )
 		{
 			ip1 = ip2;
 			ip2 = ( FloatProcessor )seq.getProcessor( i + 1 ).convertToFloat();
-			
+
 			IJ.log( "Processing slice " + i );
-			
+
 			final FloatProcessor seqFlowVectorRSlice = new FloatProcessor( imp.getWidth(), imp.getHeight() );
 			final FloatProcessor seqFlowVectorPhiSlice = new FloatProcessor( imp.getWidth(), imp.getHeight() );
 			final FloatProcessor seqRSlice = new FloatProcessor( imp.getWidth(), imp.getHeight() );
 			final ColorProcessor seqOpticFlowSlice = new ColorProcessor( imp.getWidth(), imp.getHeight() );
-			
+
 			opticFlow( ip1, ip2, seqRSlice, seqFlowVectorRSlice, seqFlowVectorPhiSlice, seqOpticFlowSlice );
-			
+
 			seqR.setPixels( seqRSlice.getPixels(), i );
 			seqR.setSliceLabel( "" + i, i );
 			seqFlowVectors.setPixels( seqFlowVectorRSlice.getPixels(), 2 * i - 1 );
@@ -300,7 +306,7 @@ public class PMCCBlockFlow implements PlugIn
 			seqFlowVectors.setSliceLabel( "phi " + i, 2 * i );
 			seqOpticFlow.setPixels( seqOpticFlowSlice.getPixels(), i );
 			seqOpticFlow.setSliceLabel( "" + i, i );
-			
+
 			if ( i == 1 )
 			{
 				impR = new ImagePlus( imp.getTitle() + " R^2", seqR );
@@ -308,29 +314,29 @@ public class PMCCBlockFlow implements PlugIn
 				impR.setCalibration( imp.getCalibration() );
 				impR.setDimensions( 1, 1, seq.getSize() - 1 );
 				impR.show();
-				
+
 				impOpticFlow = new ImagePlus( imp.getTitle() + " optic flow", seqOpticFlow );
 				impOpticFlow.setOpenAsHyperStack( true );
 				impOpticFlow.setCalibration( imp.getCalibration() );
 				impOpticFlow.setDimensions( 1, 1, seq.getSize() - 1 );
 				impOpticFlow.show();
-				
+
 				final ImagePlus notYetComposite = new ImagePlus( imp.getTitle() + " flow vectors", seqFlowVectors );
 				notYetComposite.setOpenAsHyperStack( true );
 				notYetComposite.setCalibration( imp.getCalibration() );
-				notYetComposite.setDimensions( 2, 1, seq.getSize() - 1 );				
-				
+				notYetComposite.setDimensions( 2, 1, seq.getSize() - 1 );
+
 				impFlowVectors = new CompositeImage( notYetComposite, CompositeImage.GRAYSCALE );
 				impFlowVectors.setOpenAsHyperStack( true );
 				impFlowVectors.setDimensions( 2, 1, seq.getSize() - 1 );
 				impFlowVectors.show();
-				
+
 				impFlowVectors.setPosition( 1, 1, 1 );
 				impFlowVectors.setDisplayRange( 0, 1 );
 				impFlowVectors.setPosition( 2, 1, 1 );
 				impFlowVectors.setDisplayRange( -Math.PI, Math.PI );
 			}
-			
+
 			IJ.showProgress( i, seq.getSize() );
 			impR.setSlice( i );
 			impOpticFlow.setSlice( i );
---
+++ mpicbg_/src/main/java/mpicbg/ij/plugin/PMCCScaleSpaceBlockFlow.java
@@ -0,0 +1,412 @@
+package mpicbg.ij.plugin;
+import java.util.ArrayList;
+
+import ij.CompositeImage;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.WindowManager;
+import ij.gui.GenericDialog;
+import ij.io.Opener;
+import ij.plugin.PlugIn;
+import ij.process.ColorProcessor;
+import ij.process.FloatProcessor;
+import ij.process.ShortProcessor;
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+import mpicbg.ij.integral.BlockPMCC;
+import mpicbg.ij.integral.IntegralImage;
+import mpicbg.models.NotEnoughDataPointsException;
+import mpicbg.models.Point;
+import mpicbg.models.PointMatch;
+import mpicbg.models.TranslationModel2D;
+
+/**
+ * <h1>Transfer an image sequence into an optic flow field<h1>
+ *
+ * <p>Flow fields are calculated for each pair <em>(t,t+1)</em> of the sequence
+ * independently.  The motion vector for each pixel in image t is estimated by
+ * searching the most similar looking pixel in image <em>t+1</em>.  The
+ * similarity measure is Pearson Product-Moment Correlation Coefficient of all
+ * pixels in a local vicinity.  The local vicinity is defined by a block and is
+ * calculated using an {@link IntegralImage}.  Both the size of the block and
+ * the search radius are parameters of the method.</p>
+ *
+ * @author Stephan Saalfeld <saalfeld@mpi-cbg.de>
+ * @version 0.1a
+ */
+public class PMCCScaleSpaceBlockFlow implements PlugIn
+{
+	static protected int blockRadius = 8;
+	static protected int maxDistance = 7;
+	static protected boolean showColors = false;
+
+	final static protected void algebraicToColor(
+			final short[] ipXPixels,
+			final short[] ipYPixels,
+			final int[] ipColorPixels,
+			final double max )
+	{
+		final int n = ipXPixels.length;
+		for ( int i = 0; i < n; ++i )
+		{
+			final double x = ipXPixels[ i ] / max;
+			final double y = ipYPixels[ i ] / max;
+
+			final double r = Math.sqrt( x * x + y * y );
+			final double phi = Math.atan2( x / r, y / r );
+
+			if ( r == 0.0 )
+				ipColorPixels[ i ] = 0;
+			else
+			{
+				final double red, green, blue;
+
+				double o = ( phi + Math.PI ) / Math.PI * 3;
+
+				if ( o < 3 )
+					red = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
+				else
+					red = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
+
+				o += 2;
+				if ( o >= 6 ) o -= 6;
+
+				if ( o < 3 )
+					green = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
+				else
+					green = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
+
+				o += 2;
+				if ( o >= 6 ) o -= 6;
+
+				if ( o < 3 )
+					blue = Math.min( 1.0, Math.max( 0.0, 2.0 - o ) ) * r;
+				else
+					blue = Math.min( 1.0, Math.max( 0.0, o - 4.0 ) ) * r;
+
+				ipColorPixels[ i ] =  ( ( ( ( int )( red * 255 ) << 8 ) | ( int )( green * 255 ) ) << 8 ) | ( int )( blue * 255 );
+			}
+		}
+	}
+
+	static public void opticFlow(
+			final FloatProcessor ip1,
+			final FloatProcessor ip2,
+			final ImageStack r,
+			final ImageStack shiftVectors,
+			final ImageStack of,
+			final double scaleFactor )
+	{
+		final BlockPMCC bc = new BlockPMCC( ip1.getWidth(), ip1.getHeight(), ip1, ip2 );
+		//final BlockPMCC bc = new BlockPMCC( ip1, ip2 );
+
+		final FloatProcessor ipR = bc.getTargetProcessor();
+		final float[] ipRPixels = ( float[] )ipR.getPixels();
+
+		final ArrayList< Double > radiusList = new ArrayList< Double >();
+
+		for ( double radius = 1; radius < r.getWidth() / 4; radius *= scaleFactor )
+		{
+			radiusList.add( radius );
+
+			final FloatProcessor ipRMax = new FloatProcessor( ipR.getWidth(), ipR.getHeight() );
+			final float[] ipRMaxPixels = ( float[] )ipRMax.getPixels();
+			{
+				for ( int i = 0; i < ipRMaxPixels.length; ++i )
+					ipRMaxPixels[ i ] = -1;
+			}
+			final ShortProcessor ipX = new ShortProcessor( ipR.getWidth(), ipR.getHeight() );
+			final ShortProcessor ipY = new ShortProcessor( ipR.getWidth(), ipR.getHeight() );
+			final ColorProcessor cp = new ColorProcessor( ipR.getWidth(), ipR.getHeight() );
+
+			r.addSlice( ipRMax );
+			shiftVectors.addSlice( ipX );
+			shiftVectors.addSlice( ipY );
+			of.addSlice( cp );
+		}
+
+		/* assemble into typed arrays for quicker access */
+		final float[][] rArrays = new float[ r.getSize() ][];
+		final short[][] xShiftArrays = new short[ rArrays.length ][];
+		final short[][] yShiftArrays = new short[ rArrays.length ][];
+		final int[][] ofArrays = new int[ rArrays.length ][];
+		final int[] radii = new int[ rArrays.length ];
+		for ( int i = 0; i < radii.length; ++i )
+		{
+			rArrays[ i ] = ( float[] )r.getImageArray()[ i ];
+			xShiftArrays[ i ] = ( short[] )shiftVectors.getImageArray()[ i << 1 ];
+			yShiftArrays[ i ] = ( short[] )shiftVectors.getImageArray()[ ( i << 1 ) | 1 ];
+			ofArrays[ i ] = ( int[] )of.getImageArray()[ i ];
+
+			radii[ i ] = ( int )Math.round( radiusList.get( i ) );
+		}
+
+		for ( int yo = -maxDistance; yo <= maxDistance; ++yo )
+		{
+			for ( int xo = -maxDistance; xo <= maxDistance; ++xo )
+			{
+				// continue if radius is larger than maxDistance
+				if ( yo * yo + xo * xo > maxDistance * maxDistance ) continue;
+
+				IJ.log( String.format( "(%d, %d)", xo, yo ) );
+
+				bc.setOffset( xo, yo );
+
+				for ( int ri = 0; ri < radii.length; ++ri )
+				{
+					final int blockRadius = radii[ ri ];
+
+					bc.rSignedSquare( blockRadius );
+
+					final float[] ipRMaxPixels = rArrays[ ri ];
+					final short[] ipXPixels = xShiftArrays[ ri ];
+					final short[] ipYPixels = yShiftArrays[ ri ];
+
+					// update the translation fields
+					final int h = ipR.getHeight() - maxDistance;
+					final int width = ipR.getWidth();
+					final int w = width - maxDistance;
+					for ( int y = maxDistance; y < h; ++y )
+					{
+						final int row = y * width;
+						final int rowR;
+						if ( yo < 0 )
+							rowR = row;
+						else
+							rowR = ( y - yo ) * width;
+						for ( int x = maxDistance; x < w; ++x )
+						{
+							final int i = row + x;
+							final int iR;
+							if ( xo < 0 )
+								iR = rowR + x;
+							else
+								iR = rowR + ( x - xo );
+
+							final float ipRPixel = ipRPixels[ iR ];
+							final float ipRMaxPixel = ipRMaxPixels[ i ];
+
+							if ( ipRPixel > ipRMaxPixel )
+							{
+								ipRMaxPixels[ i ] = ipRPixel;
+								ipXPixels[ i ] = ( short )xo;
+								ipYPixels[ i ] = ( short )yo;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		for ( int i = 0; i < radii.length; ++i )
+			algebraicToColor(
+					xShiftArrays[ i ],
+					yShiftArrays[ i ],
+					ofArrays[ i ],
+					maxDistance );
+	}
+
+
+	public final static void filterOpticFlowScaleSpace(
+			final ImageStack shiftVectors,
+			final ShortProcessor shiftX,
+			final ShortProcessor shiftY,
+			final ColorProcessor of,
+			final ShortProcessor inlierCounts ) throws NotEnoughDataPointsException
+	{
+		/* assemble into typed arrays for quicker access */
+		/* TODO This is still inefficient because scale dimension is fastest but should be slowest
+		 * this is true here and in opticFlow, i.e. scale should be interleaved (size of array
+		 * becomes concern, use ImgLib2).
+		 */
+		final short[][] xShiftArrays = new short[ shiftVectors.size() / 2 ][];
+		final short[][] yShiftArrays = new short[ xShiftArrays.length ][];
+		for ( int i = 0; i < xShiftArrays.length; ++i )
+		{
+			xShiftArrays[ i ] = ( short[] )shiftVectors.getImageArray()[ i << 1 ];
+			yShiftArrays[ i ] = ( short[] )shiftVectors.getImageArray()[ ( i << 1 ) | 1 ];
+		}
+
+		final int n = shiftVectors.getWidth() * shiftVectors.getHeight();
+		final int m = xShiftArrays.length;
+
+		final ArrayList< PointMatch > pq = new ArrayList< PointMatch >();
+		for ( int i = 0; i < m; ++i )
+			pq.add(
+					new PointMatch(
+							new Point( new double[]{ 0.0, 0.0 } ),
+							new Point( new double[]{ 0.0, 0.0 } ) ) );
+
+		final TranslationModel2D model = new TranslationModel2D();
+		final ArrayList< PointMatch > pqInliers = new ArrayList< PointMatch >();
+		final double[] translation = new double[ 6 ];
+
+		for ( int i = 0; i < n; ++i )
+		{
+			for ( int j = 0; j < m; ++j )
+			{
+				final double[] q = pq.get( j ).getP2().getW();
+				q[ 0 ] = xShiftArrays[ j ][ i ];
+				q[ 1 ] = yShiftArrays[ j ][ i ];
+			}
+			model.ransac( pq, pqInliers, 1000, 0.5, 0 );
+			model.toArray( translation );
+			shiftX.set( i, ( short )Math.round( translation[ 4 ] * 4 ) );
+			shiftY.set( i, ( short )Math.round( translation[ 5 ] * 4 ) );
+
+			inlierCounts.set( i, pqInliers.size() );
+
+			if ( i / inlierCounts.getWidth() * inlierCounts.getWidth() == i )
+				IJ.log( "row " + i / inlierCounts.getWidth() );
+
+		}
+
+		algebraicToColor(
+				( short[] )shiftX.getPixels(),
+				( short[] )shiftY.getPixels(),
+				(int[] )of.getPixels(),
+				maxDistance * 4 );
+
+		new ImagePlus( "ransacced", of ).show();
+		new ImagePlus( "inlierCounts", inlierCounts ).show();
+	}
+
+
+
+	@Override
+	final public void run( final String args )
+	{
+		if ( IJ.versionLessThan( "1.41n" ) ) return;
+
+		final ImagePlus imp = WindowManager.getCurrentImage();
+		if ( imp == null )  { IJ.error( "There are no images open" ); return; }
+
+		GenericDialog gd = new GenericDialog( "Generate optic flow" );
+		gd.addNumericField( "maximal_distance :", maxDistance, 0, 6, "px" );
+		gd.addCheckbox( "show_color_map", showColors );
+
+		gd.showDialog();
+
+		if (gd.wasCanceled()) return;
+
+		maxDistance = ( int )gd.getNextNumber();
+		showColors = gd.getNextBoolean();
+
+		if ( showColors )
+		{
+			ColorProcessor ipColor = new ColorProcessor( maxDistance * 2 + 1, maxDistance * 2 + 1 );
+			PMCCBlockFlow.colorCircle( ipColor );
+			ImagePlus impColor = new ImagePlus( "Color", ipColor );
+			impColor.show();
+		}
+
+
+		ImageStack seq = imp.getStack();
+		ImageStack seqR = new ImageStack( imp.getWidth(), imp.getHeight() );
+		ImageStack seqOpticFlow = new ImageStack( imp.getWidth(), imp.getHeight() );
+		ImageStack seqFlowVectors = new ImageStack( imp.getWidth(), imp.getHeight() );
+
+		FloatProcessor ip1;
+		FloatProcessor ip2 = ( FloatProcessor )seq.getProcessor( 1 ).convertToFloat();
+
+		ImagePlus impR = null;
+		ImagePlus impOpticFlow = null;
+		CompositeImage impFlowVectors = null;
+
+		for ( int i = 1; i < seq.getSize(); ++i )
+		{
+			ip1 = ip2;
+			ip2 = ( FloatProcessor )seq.getProcessor( i + 1 ).convertToFloat();
+
+			IJ.log( "Processing slice " + i );
+
+			opticFlow(
+					ip1,
+					ip2,
+					seqR,
+					seqFlowVectors,
+					seqOpticFlow,
+					1.1 );
+
+			final ShortProcessor shiftX = new ShortProcessor( imp.getWidth(), imp.getHeight() );
+			final ShortProcessor shiftY = new ShortProcessor( imp.getWidth(), imp.getHeight() );
+			final ColorProcessor of = new ColorProcessor( imp.getWidth(), imp.getHeight() );
+			final ShortProcessor inlierCounts = new ShortProcessor( imp.getWidth(), imp.getHeight() );
+			try
+			{
+				filterOpticFlowScaleSpace(
+						seqFlowVectors,
+						shiftX,
+						shiftY,
+						of,
+						inlierCounts );
+			}
+			catch ( NotEnoughDataPointsException e )
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if ( i == 1 )
+			{
+				impR = new ImagePlus( imp.getTitle() + " R^2", seqR );
+				impR.setOpenAsHyperStack( true );
+				impR.setCalibration( imp.getCalibration() );
+				impR.setDimensions( 1, 1, seq.getSize() - 1 );
+				impR.show();
+
+				impOpticFlow = new ImagePlus( imp.getTitle() + " optic flow", seqOpticFlow );
+				impOpticFlow.setOpenAsHyperStack( true );
+				impOpticFlow.setCalibration( imp.getCalibration() );
+				impOpticFlow.setDimensions( 1, 1, seq.getSize() - 1 );
+				impOpticFlow.show();
+
+				final ImagePlus notYetComposite = new ImagePlus( imp.getTitle() + " flow vectors", seqFlowVectors );
+				notYetComposite.setOpenAsHyperStack( true );
+				notYetComposite.setCalibration( imp.getCalibration() );
+				notYetComposite.setDimensions( 2, 1, seq.getSize() - 1 );
+
+				impFlowVectors = new CompositeImage( notYetComposite, CompositeImage.GRAYSCALE );
+				impFlowVectors.setOpenAsHyperStack( true );
+				impFlowVectors.setDimensions( 2, 1, seq.getSize() - 1 );
+				impFlowVectors.show();
+
+				impFlowVectors.setPosition( 1, 1, 1 );
+				impFlowVectors.setDisplayRange( 0, 1 );
+				impFlowVectors.setPosition( 2, 1, 1 );
+				impFlowVectors.setDisplayRange( -Math.PI, Math.PI );
+			}
+
+			IJ.showProgress( i, seq.getSize() );
+			impR.setSlice( i );
+			impOpticFlow.setSlice( i );
+			impFlowVectors.setPosition( 1, 1, i );
+			imp.setSlice( i + 1 );
+		}
+	}
+
+	public final static void main( final String... args )
+	{
+		new ImageJ();
+		final ImagePlus imp = new Opener().openImage( "/home/saalfeld/tmp/scheffer/flow/stack.0.025.tif" );
+		imp.show();
+		new PMCCScaleSpaceBlockFlow().run("");
+	}
+}
